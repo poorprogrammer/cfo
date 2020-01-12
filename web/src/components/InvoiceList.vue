836dd00bfc11f96d6850259a9a9ac1dbabad67b1
jane <jane@odd-e.comjane@odd-e.com>
2020-01-12 17:16:24 +1100

    Consume api with hardcoded url

--- web/src/components/InvoiceList.vue
+++ web/src/components/InvoiceList.vue
@@ -1,14 +1,35 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
+
+    <table>
+      <caption>The Beatles</caption>
+      <tr v-for="invoice in invoices">
+        <td>{{ invoice.invoiceDate }}</td>
+        <td>{{ invoice.amount }}</td>
+        <td>{{ invoice.invoiceNumber }}</td>
+      </tr>
+    </table>
   </div>
 </template>
 
 <script>
+import axios from 'axios'
+
 export default {
   name: 'InvoiceList',
   props: {
     msg: String
+  },
+  data() {
+    return {
+      invoices: null
+    }
+  },
+  mounted() {
+    axios
+      .get('http://localhost:3000/spacex/invoices')
+      .then(response => (this.invoices = response.data))
   }
 }
 </script>
