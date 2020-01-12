cc6672cd6ee95dd5763cc5bf95e5b0feae95f135
juacompe <juacompe@gmail.com>
2020-01-12 16:43:31 +1100

    add /spacex/invoices

--- api/app.js
+++ api/app.js
@@ -3,5 +3,30 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/spacex/invoices', (req, res) => {
+    let invoices = [
+        {
+            invoiceNumber: '202001001',
+            amount: 130000,
+            invoiceDate: '2020-01-03'
+        },
+        {
+            invoiceNumber: '202001003',
+            amount: 80000,
+            invoiceDate: '2020-01-07'
+        },
+        {
+            invoiceNumber: '202001004',
+            amount: 100000,
+            invoiceDate: '2020-01-07'
+        },
+        {
+            invoiceNumber: '202001005',
+            amount: 150000,
+            invoiceDate: '2020-01-07'
+        },
+    ]
+    res.json(invoices)
+})
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
