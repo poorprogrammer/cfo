c106826f2f2c047d60114d3597ea76a6193cf353
jane <jane@odd-e.comjane@odd-e.com>
2020-01-12 16:42:15 +1100

    Change title for Home page

---
+++ web/tests/e2e/specs/home.test.js
@@ -0,0 +1,8 @@
+// https://docs.cypress.io/api/introduction/api.html
+
+describe('Home Page Now', () => {
+  it('Visits the app root url should see homepage and proper title', () => {
+    cy.visit('/')
+    cy.contains('h1', 'Invoice List')
+  })
+})
