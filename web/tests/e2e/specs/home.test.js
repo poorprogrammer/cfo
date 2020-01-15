a0f7f1623be283a6484a727fca47ffe1afb996f6
jane <jane@odd-e.comjane@odd-e.com>
2020-01-16 02:00:09 +1100

    Fixed e2e after change title

--- web/tests/e2e/specs/home.test.js
+++ web/tests/e2e/specs/home.test.js
@@ -3,7 +3,6 @@
 describe('Home Page Now', () => {
   it('Visits the app root url should see homepage and proper title', () => {
     cy.visit('/')
-    cy.contains('h1', 'Invoice List')
     cy.contains('202001-001')
   })
 })
