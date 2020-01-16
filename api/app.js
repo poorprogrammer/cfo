4c4cc37b13041c4a40db3695fb45130010831413
jane <jane@odd-e.comjane@odd-e.com>
2020-01-16 23:21:54 +1100

    Fiexd Security Hotspots warning

--- api/app.js
+++ api/app.js
@@ -3,7 +3,18 @@ const app = express()
 const port = 3000
 const cors = require('cors')
 
-app.use(cors())
+const whitelist = ['http://localhost:8080']
+const corsOptions = {
+    origin: function (origin, callback) {
+        if (whitelist.indexOf(origin) !== -1) {
+            callback(null, true)
+        } else {
+            callback(new Error('Not allowed by CORS'))
+        }
+    }
+}
+
+app.use(cors(corsOptions))
 app.get('/', (req, res) => res.send('Hello World!'))
 app.get('/spacex/invoices', (req, res) => {
     let invoices = [
