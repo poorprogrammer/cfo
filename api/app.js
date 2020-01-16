2c518b031e197a6f394429c67c6f06c1140b6b22
jane <jane@odd-e.comjane@odd-e.com>
2020-01-16 23:29:53 +1100

    Revert "Fiexd Security Hotspots warning"
    This reverts commit 4c4cc37b13041c4a40db3695fb45130010831413.

--- api/app.js
+++ api/app.js
@@ -3,18 +3,7 @@ const app = express()
 const port = 3000
 const cors = require('cors')
 
-const whitelist = ['http://localhost:8080']
-const corsOptions = {
-    origin: function (origin, callback) {
-        if (whitelist.indexOf(origin) !== -1) {
-            callback(null, true)
-        } else {
-            callback(new Error('Not allowed by CORS'))
-        }
-    }
-}
-
-app.use(cors(corsOptions))
+app.use(cors())
 app.get('/', (req, res) => res.send('Hello World!'))
 app.get('/spacex/invoices', (req, res) => {
     let invoices = [
