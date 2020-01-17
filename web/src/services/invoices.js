da4bc3388ee092009a90c069c68199752e7c36db
juacompe <juacompe@gmail.com>
2020-01-18 00:39:52 +1100

    Format document

--- web/src/services/invoices.js
+++ web/src/services/invoices.js
@@ -1,14 +1,12 @@
-import axios from 'axios'
+import axios from "axios";
 
 export default class {
-    invoicesUrl () {
-        return process.env.VUE_APP_BASE_API + '/spacex/invoices'
-    }
-
-    getInvoices () {
-        return axios
-                .get(this.invoicesUrl())
-                .then(response => (response.data))
-    }
+  invoicesUrl() {
+    return process.env.VUE_APP_BASE_API + "/spacex/invoices";
   }
-  
+
+  getInvoices() {
+    return axios.get(this.invoicesUrl())
+                .then(response => response.data);
+  }
+}
