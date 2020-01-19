064bf5a638bebbd052168210b47eff1cb63c60b8
jane <jane@odd-e.comjane@odd-e.com>
2020-01-19 21:06:03 +0700

    Refucktor: move task in init into contructure

--- web/src/presenters/InvoiceList.js
+++ web/src/presenters/InvoiceList.js
@@ -2,27 +2,27 @@ import API from '@/services/invoices'
 import Invoice from '@/models/Invoice'
 
 export default class {
-    constructor (view) {
-      this.view = view
-      this.invoices = []
-      this.headers = [
-        { text: 'Number', value: 'invoiceNumber' },
-        { text: 'Company', value: 'companySlug' },
-        { text: 'Date', value: 'invoiceDate' },
-      ]
-      this.API = new API()
-      this.API.init()
-    }
-    init () {
-      this.API.getInvoices()
-        .then(this.setInvoices)
-    }
-    setInvoices = (invoices) => {
-      this.invoices = []
-      invoices.forEach(invoice => {
-        this.invoices.push(new Invoice(invoice))
-      })
-    }
-    sortBy = () => ("invoiceNumber")
-    sortDesc = () => (true)
+  constructor(view) {
+    this.view = view
+    this.invoices = []
+    this.headers = [
+      { text: 'Number', value: 'invoiceNumber' },
+      { text: 'Company', value: 'companySlug' },
+      { text: 'Date', value: 'invoiceDate' },
+    ]
+    this.API = new API()
+    // this.API.init()
+  }
+  init() {
+    this.API.getInvoices()
+      .then(this.setInvoices)
+  }
+  setInvoices = (invoices) => {
+    this.invoices = []
+    invoices.forEach(invoice => {
+      this.invoices.push(new Invoice(invoice))
+    })
+  }
+  sortBy = () => ("invoiceNumber")
+  sortDesc = () => (true)
 }
--- web/src/services/invoices.js
+++ web/src/services/invoices.js
@@ -1,16 +1,16 @@
 import axios from "axios";
 
 export default class {
-  constructor() { this.root = '' }
-
-  init() { this.root = process.env.VUE_APP_BASE_API }
+  constructor() {
+    this.root = process.env.VUE_APP_BASE_API
+  }
 
   invoicesUrl() {
-    return  `${this.root}/invoices/2020`
+    return `${this.root}/invoices/2020`
   }
 
   getInvoices() {
     return axios.get(this.invoicesUrl())
-                .then(response => response.data);
+      .then(response => response.data);
   }
 }
