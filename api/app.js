44d4a52f4ed7133d599a07bd10836a616c93ccfa
jane <jane@odd-e.comjane@odd-e.com>
2020-01-19 19:08:34 +0700

    Separate server config from app file
    ref: https://github.com/goldbergyoni/nodebestpractices/blob/master/sections/projectstructre/separateexpress.md

--- api/app.js
+++ api/app.js
@@ -1,6 +1,5 @@
 const express = require('express')
 const app = express()
-const port = 3000
 const cors = require('cors')
 
 app.use(cors())
@@ -53,4 +52,4 @@ app.get('/invoices/2020', (req, res) => {
     res.json(invoices)
 })
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+module.exports = app
