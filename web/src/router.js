2e8f8d6bf7a9e26bba5842c3fcab1194e82866e0
juacompe <juacompe@gmail.com>
2020-01-19 09:01:29 +0700

    Add empty view invoice page

--- web/src/router.js
+++ web/src/router.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from './views/Home.vue'
+import Invoice from './views/Invoice.vue'
 
 Vue.use(Router)
 
@@ -12,6 +13,11 @@ export default new Router({
       path: '/',
       name: 'home',
       component: Home
+    },
+    {
+      path: '/:invoiceNumber/invoice',
+      name: 'invoice',
+      component: Invoice
     },
     {
       path: '/about',
---
+++ web/src/views/Invoice.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="invoice">
+    <v-card>
+      <v-card-title>Invoice</v-card-title>
+    </v-card>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'invoice',
+}
+</script>
