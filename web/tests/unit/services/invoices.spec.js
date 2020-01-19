6f6819782f13131a0a33c2b3b32ca22da72e5b76
jane <jane@odd-e.comjane@odd-e.com>
2020-01-19 23:16:32 +0700

    Added more unit test

--- web/tests/unit/services/invoices.spec.js
+++ web/tests/unit/services/invoices.spec.js
@@ -1,9 +1,19 @@
 import API from '@/services/invoices'
-
+import axios from 'axios'
 describe('invoices API', () => {
+  let api;
+
+  beforeEach(() => {
+    api = new API()
+  })
 
   it('should be empty list initally or the screen would not render', () => {
-    let api = new API()
     expect(api.invoicesUrl()).toEqual('http://api:3000/invoices/2020')
+  })
+
+  it('should retrive list of invoice from proper api', () => {
+    jest.spyOn(axios, 'get')
+    api.getInvoices();
+    expect(axios.get).toHaveBeenCalled();
   })
 })
