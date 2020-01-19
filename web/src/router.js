f86ce73631aa4195be881a1f2b9dd3686b2eb358
jane <jane@odd-e.comjane@odd-e.com>
2020-01-19 20:03:59 +0700

    Introduce Page not found

--- web/src/router.js
+++ web/src/router.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Home from './views/Home.vue'
 import Invoice from './views/Invoice.vue'
+import PageNotFound from './views/PageNotFound.vue'
 
 Vue.use(Router)
 
@@ -26,6 +27,10 @@ export default new Router({
       // this generates a separate chunk (about.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
+    },
+    {
+      path: '*',
+      component: PageNotFound
     }
   ]
 })
