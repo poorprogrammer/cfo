479771904f137eb18aaef04b5fad988747e04841
juacompe <juacompe@gmail.com>
2020-01-23 15:37:07 +0700

    Add api coverage

--- web/tests/e2e/specs/home.test.js
+++ web/tests/e2e/specs/home.test.js
@@ -1,7 +1,7 @@
 // https://docs.cypress.io/api/introduction/api.html
 
 describe('Home Page Now', () => {
-  it('Visits the app root url should see homepage and proper title', () => {
+  it('Visits the app root url should see latest invoice number', () => {
     cy.visit('/')
     cy.contains('202001-007')
   })
