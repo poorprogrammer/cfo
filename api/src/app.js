bec27f1fa01005e0f6bb18192aab0b2f3a7a13d3
juacompe <juacompe@gmail.com>
2020-01-23 16:03:51 +0700

    Refactor invoices service out of app

--- api/src/app.js
+++ api/src/app.js
@@ -1,55 +1,12 @@
 const express = require('express')
 const app = express()
 const cors = require('cors')
+const invoices = require('./services/invoices')
 
 app.use(cors())
 app.get('/', (req, res) => res.send('Hello World!'))
 app.get('/invoices/2020', (req, res) => {
-    let invoices = [
-        {
-            amount: 130000,
-            companySlug: 'spacex',
-            invoiceDate: '2020-01-03',
-            invoiceNumber: '202001-001',
-        },
-        {
-            amount: 70000,
-            companySlug: 'google',
-            invoiceDate: '2020-01-04',
-            invoiceNumber: '202001-002',
-        },
-        {
-            amount: 80000,
-            companySlug: 'spacex',
-            invoiceDate: '2020-01-07',
-            invoiceNumber: '202001-003',
-        },
-        {
-            amount: 100000,
-            companySlug: 'spacex',
-            invoiceDate: '2020-01-07',
-            invoiceNumber: '202001-004',
-        },
-        {
-            amount: 150000,
-            companySlug: 'spacex',
-            invoiceDate: '2020-01-07',
-            invoiceNumber: '202001-005',
-        },
-        {
-            amount: 300000,
-            companySlug: 'facebook',
-            invoiceDate: '2020-01-04',
-            invoiceNumber: '202001-006',
-        },
-        {
-            amount: 450000,
-            companySlug: 'facebook',
-            invoiceDate: '2020-01-04',
-            invoiceNumber: '202001-007',
-        },
-    ]
-    res.json(invoices)
+    res.json(invoices())
 })
 
 module.exports = app
