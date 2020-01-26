9e5b65f91148c3dda41ff410d71968a0d96b7bdd
juacompe <juacompe@gmail.com>
2020-01-26 20:58:13 +0700

    Refactor

--- web/tests/unit/services/invoices.spec.js
+++ web/tests/unit/services/invoices.spec.js
@@ -3,24 +3,33 @@ import axios from 'axios'
 describe('invoices API', () => {
   let api;
 
-  beforeEach(() => {
-    api = new API()
-  })
+  beforeEach(() => { api = new API() })
 
-  it('should have invoice url as /invoices/:year', () => {
-    expect(api.invoicesUrl()).toEqual('http://api:3000/invoices/2020')
-  })
+  describe('getInvoices', () => {
+    describe('before call', () => {
+      beforeEach(() => {
+        jest.spyOn(axios, 'get')
+        api.getInvoices()
+      })
 
-  it('should retrive list of invoice from proper api', () => {
-    jest.spyOn(axios, 'get')
-    api.getInvoices()
-    expect(axios.get).toHaveBeenCalled()
-  })
+      it('should call api', () => {
+        expect(axios.get).toHaveBeenCalled()
+      })
+      it('should call with correct url', () => {
+        expect(axios.get.mock.calls[0][0]).toEqual('http://api:3000/invoices/2020')
+      })
+    })
 
-  it('should parse response into Invoice', async() => {
-    let response = { data: [{invoiceNumber: '202001-001'}] }
-    jest.spyOn(axios, 'get').mockResolvedValue(response)
-    let invoices = await api.getInvoices()
-    expect(invoices[0].url()).toEqual('/invoice/202001-001')
+    describe('after call', () => {
+      beforeEach(() => {
+        let response = { data: [{invoiceNumber: '202001-001'}] }
+        jest.spyOn(axios, 'get').mockResolvedValue(response)
+      })
+
+      it('should parse response into Invoice', async() => {
+        let invoices = await api.getInvoices()
+        expect(invoices[0].url()).toEqual('/invoice/202001-001')
+      })
+    })
   })
 })
