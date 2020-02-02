647aab6cc4cc933cf6bc87b7d9036b7e4224ef13
juacompe <juacompe@gmail.com>
2020-02-02 21:13:40 +0700

    Refactor

--- web/src/models/Invoice.js
+++ web/src/models/Invoice.js
@@ -16,8 +16,8 @@ export default class Invoice {
   }
   url() { return `/invoice/${this.invoiceNumber}` }
   getItems() { return [...this.items, this.total(), this.tax(), this.grandTotal()] }
-  total() { return new Total(this.getTotal()) }
-  tax() { return new Tax(this.getTotal()) }
+  total() { return new CalculatedItem('Total', this.getTotal()) }
+  tax() { return new CalculatedItem('Tax', this.getTotal() * 0.07) }
   grandTotal() { return new CalculatedItem('Grand Total', this.getTotal() * 1.07)}
   getTotal() { return this.items.map(getItemTotal).reduce(sum) }
 }
@@ -31,23 +31,4 @@ class CalculatedItem {
     this.t = t
   }
   total() { return this.t }
-}
-class Total {
-  constructor(t) {
-    this.name = 'Total'
-    this.price = ''
-    this.amount = ''
-    this.t = t
-  }
-  total() { return this.t }
-}
-
-class Tax {
-  constructor(t) {
-    this.name = 'Tax'
-    this.price = ''
-    this.amount = ''
-    this.t = t
-  }
-  total() { return this.t * 0.07 }
 }
