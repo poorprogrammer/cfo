8325f6f620db9aa9ccc96b5d236159476027308e
juacompe <juacompe@gmail.com>
2020-02-06 05:31:02 +0700

    Fix failing test

--- web/tests/e2e/specs/invoice.test.js
+++ web/tests/e2e/specs/invoice.test.js
@@ -9,8 +9,19 @@ describe('Invoice Page Now', () => {
   }
   function containsOriginalInvoice(cy) {
     cy.contains('Invoice (original)')
-    cy.contains('Invoice Number: 202001-001')
-    cy.contains('Company: spacex')
-    cy.contains('Invoice Date: 2020-01-03')
+    containsTargetCompanyName(cy)
+    containsInvoiceNumber(cy)
+    containsInvoiceDate(cy)
+  }
+  function containsTargetCompanyName(cy) {
+    cy.contains('SPACEX HEADQUARTERS')
+  }
+  function containsInvoiceNumber(cy) {
+    cy.contains('Invoice Number')
+    cy.contains('202001-001')
+  }
+  function containsInvoiceDate(cy) {
+    cy.contains('Invoice Date')
+    cy.contains('2020-01-03')
   }
 })
