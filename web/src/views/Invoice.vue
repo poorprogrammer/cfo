6be8e09cc2d947cee41625c4b2c6c013e94008de
juacompe <juacompe@gmail.com>
2020-02-06 01:29:57 +0700

    Align text

--- web/src/views/Invoice.vue
+++ web/src/views/Invoice.vue
@@ -2,33 +2,35 @@
   <div class="invoice">
     <v-card>
       <v-card-title>Invoice (original)</v-card-title>
+      <v-card-text>
+        <div v-if="!p.invoice.invoiceNumber">Loading Please wait...</div>
+        <div v-else>
+          <p>Invoice Number: {{ p.invoice.invoiceNumber }}</p>
+          <p>Company: {{ p.invoice.companySlug }}</p>
+          <p>Invoice Date: {{ p.invoice.invoiceDate }}</p>
+          <v-simple-table>
+            <template v-slot:default>
+              <thead>
+                <tr>
+                  <th scope="col" class="text-left">Item</th>
+                  <th scope="col" class="text-right">Price</th>
+                  <th scope="col" class="text-right">Amount</th>
+                  <th scope="col" class="text-right">Total</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr v-for="item in p.invoice.getItems()" :key="item.name">
+                  <td class="text-left">{{ item.name }}</td>
+                  <td class="text-right">{{ item.price }}</td>
+                  <td class="text-right">{{ item.amount }}</td>
+                  <td class="text-right">{{ item.total() }}</td>
+                </tr>
+              </tbody>
+            </template>
+          </v-simple-table>
+        </div>
+      </v-card-text>
     </v-card>
-    <div v-if="!p.invoice.invoiceNumber">Loading Please wait...</div>
-    <div v-else>
-      <p>Invoice Number: {{ p.invoice.invoiceNumber }}</p>
-      <p>Company: {{ p.invoice.companySlug }}</p>
-      <p>Invoice Date: {{ p.invoice.invoiceDate }}</p>
-      <v-simple-table>
-        <template v-slot:default>
-          <thead>
-            <tr>
-              <th scope="col" class="text-left">Item</th>
-              <th scope="col" class="text-right">Price</th>
-              <th scope="col" class="text-right">Amount</th>
-              <th scope="col" class="text-right">Total</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr v-for="item in p.invoice.getItems()" :key="item.name">
-              <td class="text-left">{{ item.name }}</td>
-              <td class="text-right">{{ item.price }}</td>
-              <td class="text-right">{{ item.amount }}</td>
-              <td class="text-right">{{ item.total() }}</td>
-            </tr>
-          </tbody>
-        </template>
-      </v-simple-table>
-    </div>
   </div>
 </template>
 
