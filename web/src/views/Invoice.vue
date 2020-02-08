3ce35d73e6e94420b8771ce11f846c18f6eba5b2
juacompe <juacompe@gmail.com>
2020-02-08 22:44:04 +0700

    Fix indent

--- web/src/views/Invoice.vue
+++ web/src/views/Invoice.vue
@@ -1,55 +1,55 @@
 <template>
   <div class="invoice pa-3">
-      <h2>Invoice (original)</h2>
-        <div v-if="!p.invoice.invoiceNumber">Loading Please wait...</div>
-        <div v-else>
-          <v-row align="stretch" dense>
-            <v-col cols="6">
-              <v-card class="pa-2" outlined>
-                <v-card-title class="headline">{{ p.invoice.getTargetCompanyName() }}</v-card-title>
-                <v-card-text>
-                  <div class="text--primary">{{ p.invoice.getTargetCompanyAddress() }}</div>
-                  <p><v-row dense>
-                    <v-col cols="6" sm="3"><v-chip color="primary" outlined>Tax Id</v-chip></v-col>
-                    <v-col cols="6" sm="3">{{ p.invoice.getTargetCompanyTaxId() }}</v-col>
-                    <v-col cols="6" sm="3"><v-chip color="primary" outlined>Tel</v-chip></v-col>
-                    <v-col cols="6" sm="3">{{ p.invoice.getTargetCompanyTel() }}</v-col>
-                  </v-row></p>
-                </v-card-text>
-              </v-card>
-            </v-col>
-            <v-col cols="6">
-              <v-card class="pa-2" outlined>
-                <p><v-row dense>
-                  <v-col cols="6" sm="3"><v-chip color="primary" outlined>Invoice Number</v-chip></v-col>
-                  <v-col cols="6" sm="3">{{ p.invoice.invoiceNumber }}</v-col>
-                  <v-col cols="6" sm="3"><v-chip color="primary" outlined>Invoice Date</v-chip></v-col>
-                  <v-col cols="6" sm="3">{{ p.invoice.invoiceDate }}</v-col>
-                </v-row></p>
-              </v-card>
-            </v-col>
-          </v-row>
-          <v-simple-table>
-            <template v-slot:default>
-              <thead>
-                <tr>
-                  <th scope="col" class="text-left">Item</th>
-                  <th scope="col" class="text-right">Price</th>
-                  <th scope="col" class="text-right">Amount</th>
-                  <th scope="col" class="text-right">Total</th>
-                </tr>
-              </thead>
-              <tbody>
-                <tr v-for="item in p.invoice.getItems()" :key="item.name">
-                  <td class="text-left">{{ item.name }}</td>
-                  <td class="text-right">{{ item.getPrice() }}</td>
-                  <td class="text-right">{{ item.amount }}</td>
-                  <td class="text-right">{{ item.getTotal() }}</td>
-                </tr>
-              </tbody>
-            </template>
-          </v-simple-table>
-        </div>
+    <h2>Invoice (original)</h2>
+    <div v-if="!p.invoice.invoiceNumber">Loading Please wait...</div>
+    <div v-else>
+      <v-row align="stretch" dense>
+        <v-col cols="6">
+          <v-card class="pa-2" outlined>
+            <v-card-title class="headline">{{ p.invoice.getTargetCompanyName() }}</v-card-title>
+            <v-card-text>
+              <div class="text--primary">{{ p.invoice.getTargetCompanyAddress() }}</div>
+              <p><v-row dense>
+                <v-col cols="6" sm="3"><v-chip color="primary" outlined>Tax Id</v-chip></v-col>
+                <v-col cols="6" sm="3">{{ p.invoice.getTargetCompanyTaxId() }}</v-col>
+                <v-col cols="6" sm="3"><v-chip color="primary" outlined>Tel</v-chip></v-col>
+                <v-col cols="6" sm="3">{{ p.invoice.getTargetCompanyTel() }}</v-col>
+              </v-row></p>
+            </v-card-text>
+          </v-card>
+        </v-col>
+        <v-col cols="6">
+          <v-card class="pa-2" outlined>
+            <p><v-row dense>
+              <v-col cols="6" sm="3"><v-chip color="primary" outlined>Invoice Number</v-chip></v-col>
+              <v-col cols="6" sm="3">{{ p.invoice.invoiceNumber }}</v-col>
+              <v-col cols="6" sm="3"><v-chip color="primary" outlined>Invoice Date</v-chip></v-col>
+              <v-col cols="6" sm="3">{{ p.invoice.invoiceDate }}</v-col>
+            </v-row></p>
+          </v-card>
+        </v-col>
+      </v-row>
+      <v-simple-table>
+        <template v-slot:default>
+          <thead>
+            <tr>
+              <th scope="col" class="text-left">Item</th>
+              <th scope="col" class="text-right">Price</th>
+              <th scope="col" class="text-right">Amount</th>
+              <th scope="col" class="text-right">Total</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr v-for="item in p.invoice.getItems()" :key="item.name">
+              <td class="text-left">{{ item.name }}</td>
+              <td class="text-right">{{ item.getPrice() }}</td>
+              <td class="text-right">{{ item.amount }}</td>
+              <td class="text-right">{{ item.getTotal() }}</td>
+            </tr>
+          </tbody>
+        </template>
+      </v-simple-table>
+    </div>
   </div>
 </template>
 
