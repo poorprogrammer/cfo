d7d534b65a2fba2929010b55e85146e5d9c4d7de
juacompe <juacompe@gmail.com>
2020-02-09 20:54:25 +0700

    Add more e2e tests
    ref: https://github.com/cypress-io/cypress/issues/1393 for checking css

--- web/tests/e2e/specs/invoice.test.js
+++ web/tests/e2e/specs/invoice.test.js
@@ -9,9 +9,18 @@ describe('Invoice Page Now', () => {
   }
   function containsOriginalInvoice(cy) {
     cy.contains('Invoice (original)')
+    containsFromCompanyLogo(cy)
+    containsFromCompanyName(cy)
     containsTargetCompanyName(cy)
     containsInvoiceNumber(cy)
     containsInvoiceDate(cy)
+    containsSignaturesWhenPrint(cy)
+  }
+  function containsFromCompanyLogo(cy) {
+    cy.get('#from-company-logo')
+  }
+  function containsFromCompanyName(cy) {
+    cy.contains('ODDS HQ')
   }
   function containsTargetCompanyName(cy) {
     cy.contains('SPACEX HEADQUARTERS')
@@ -23,5 +32,11 @@ describe('Invoice Page Now', () => {
   function containsInvoiceDate(cy) {
     cy.contains('Invoice Date')
     cy.contains('2020-01-03')
+  }
+  function containsSignaturesWhenPrint(cy) {
+    cy.get('#signatures').should(onlyVisibleWhenPrint)
+  }
+  function onlyVisibleWhenPrint($el) {
+    expect($el).to.have.css('display', 'none')
   }
 })
