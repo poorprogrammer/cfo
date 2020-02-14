916ca3135a7ebbe2186b1405295e60d7ad1117c1
jane <jane@odd-e.comjane@odd-e.com>
2020-02-15 02:01:41 +0900

    Fixed currency display differently

--- web/src/models/InvoiceItem.js
+++ web/src/models/InvoiceItem.js
@@ -1,9 +1,20 @@
 export default class InvoiceItem {
+
   constructor(name, t) {
     this.name = name
     this.t = t
   }
-  getTotal() { return InvoiceItem.getCurrency(this.t)}
-  getPrice() { return '' }
-  static getCurrency(n) { return new Intl.NumberFormat('th-TH', {style: 'currency', currency: 'THB'}).format(n) }
+
+  getTotal() {
+    return InvoiceItem.getCurrency(this.t)
+  }
+
+  getPrice() {
+    return ''
+  }
+
+  static getCurrency(n) {
+    return new Intl.NumberFormat('th-TH', { style: 'currency', currency: 'THB', currencyDisplay: 'code' }).format(n)
+  }
+
 }
