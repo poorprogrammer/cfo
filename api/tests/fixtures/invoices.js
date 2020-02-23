9193abc6f4ded3d222b30689720b6520b63c14a9
juacompe <juacompe@gmail.com>
2020-02-23 10:04:52 +0700

    Test fixture

---
+++ api/tests/fixtures/invoices.js
@@ -0,0 +1,138 @@
+const Database = require("../../src/persistence/nedb")
+
+let invoices = [
+    {
+        amount: 130000,
+        fromCompany: {
+            name: "ODDS HQ",
+            address: "69 We are not hiring",
+            taxId: "0100000000000",
+            tel: "+66896669999"
+        },
+        projectName: "Dragon",
+        targetCompany: {
+            name: "SPACEX HEADQUARTERS",
+            address: "Rocket Road Hawthorne, California",
+            taxId: "0100000000000",
+            tel: "310-363-6000"
+        },
+        invoiceDate: "2020-01-03",
+        invoiceNumber: "202001-001",
+        items: [{ name: "Training", price: 299, amount: 50 }]
+    },
+    {
+        amount: 70000,
+        fromCompany: {
+            name: "ODDS HQ",
+            address: "69 We are not hiring",
+            taxId: "0100000000000",
+            tel: "+66896669999"
+        },
+        projectName: "Google Map",
+        targetCompany: {
+            name: "Googleplex",
+            address:
+                "1600 Amphitheatre Parkway in Mountain View, California, United States",
+            taxId: "0000000000000",
+            tel: ""
+        },
+        invoiceDate: "2020-01-04",
+        invoiceNumber: "202001-002"
+    },
+    {
+        amount: 80000,
+        fromCompany: {
+            name: "ODDS HQ",
+            address: "69 We are not hiring",
+            taxId: "0100000000000",
+            tel: "+66896669999"
+        },
+        projectName: "Dragon II",
+        targetCompany: {
+            name: "SPACEX HEADQUARTERS",
+            address: "Rocket Road Hawthorne, California",
+            taxId: "0100000000000",
+            tel: "310-363-6000"
+        },
+        invoiceDate: "2020-01-07",
+        invoiceNumber: "202001-003"
+    },
+    {
+        amount: 100000,
+        fromCompany: {
+            name: "ODDS HQ",
+            address: "69 We are not hiring",
+            taxId: "0100000000000",
+            tel: "+66896669999"
+        },
+        projectName: "Mars transportation",
+        targetCompany: {
+            name: "SPACEX HEADQUARTERS",
+            address: "Rocket Road Hawthorne, California",
+            taxId: "0100000000000",
+            tel: "310-363-6000"
+        },
+        invoiceDate: "2020-01-07",
+        invoiceNumber: "202001-004"
+    },
+    {
+        amount: 150000,
+        fromCompany: {
+            name: "ODDS HQ",
+            address: "69 We are not hiring",
+            taxId: "0100000000000",
+            tel: "+66896669999"
+        },
+        projectName: "Falcon",
+        targetCompany: {
+            name: "SPACEX HEADQUARTERS",
+            address: "Rocket Road Hawthorne, California",
+            taxId: "0100000000000",
+            tel: "310-363-6000"
+        },
+        invoiceDate: "2020-01-07",
+        invoiceNumber: "202001-005"
+    },
+    {
+        amount: 300000,
+        fromCompany: {
+            name: "ODDS HQ",
+            address: "69 We are not hiring",
+            taxId: "0100000000000",
+            tel: "+66896669999"
+        },
+        projectName: "GraphQL",
+        targetCompany: {
+            name: "Facebook HQ",
+            address: "1601 Willow Rd Menlo Park, California",
+            taxId: "0100008000007",
+            tel: "+1 650-960-1300"
+        },
+        invoiceDate: "2020-01-04",
+        invoiceNumber: "202001-006"
+    },
+    {
+        amount: 450000,
+        fromCompany: {
+            name: "ODDS HQ",
+            address: "69 We are not hiring",
+            taxId: "0100000000000",
+            tel: "+66896669999"
+        },
+        projectName: "React",
+        targetCompany: {
+            name: "Facebook HQ",
+            address: "1601 Willow Rd Menlo Park, California",
+            taxId: "0100008000007",
+            tel: "+1 650-960-1300"
+        },
+        invoiceDate: "2020-01-04",
+        invoiceNumber: "202001-007",
+        items: [
+            { name: "Developer", price: 20000, amount: 20 },
+            { name: "Scrum master", price: 80, amount: 10 }
+        ]
+    }
+]
+
+Database.insert(invoices)
