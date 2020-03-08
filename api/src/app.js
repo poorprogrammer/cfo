e7f0d8631b3b8643f05722733d1f0e98f7271a3d
juacompe <juacompe@gmail.com>
2020-03-08 08:31:23 +0700

    Refactor
    Move services.invoices into services.Invoice.all

--- api/src/app.js
+++ api/src/app.js
@@ -1,13 +1,12 @@
 const express = require('express')
 const app = express()
 const cors = require('cors')
-const invoices = require('./services/invoices')
 const Invoice = require('./services/invoice')
 
 app.use(cors())
 app.get('/', (req, res) => res.send('Hello World!'))
 app.get('/invoices/2020', (req, res) => {
-    res.json(invoices())
+    res.json(new Invoice().all())
 })
 app.get('/invoice/:invoiceNumber', function (req, res) {
     res.json(new Invoice().get(req.params.invoiceNumber))
