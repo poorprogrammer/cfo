558d2d5efd56c158d2580ee2967a0e4df32b4f38
juacompe <juacompe@gmail.com>
2020-12-27 14:45:56 +0700

    format document

--- web/src/views/Invoice.vue
+++ web/src/views/Invoice.vue
@@ -7,135 +7,135 @@
         :key="id"
         :class="css"
       >
-      <v-row justify="center" dense>
-        <h2>{{ title }}</h2>
-      </v-row>
-      <v-row align="stretch" dense>
-        <v-col cols="12">
-          <v-card class="pa-2" outlined>
-            <v-list-item three-line>
-              <v-list-item-content>
-                <div class="overline mb-4">From Company</div>
-                <v-list-item-title class="title mb-1">{{
-                  p.invoice.getFromCompanyName()
-                }}</v-list-item-title>
-                <v-list-item-subtitle class="text--primary">
-                  <p class="text--primary">
-                    {{ p.invoice.getFromCompanyAddress() }}
-                  </p>
-                  <p>
-                    <v-row dense>
-                      <v-col cols="3" sm="2">
-                        <span class="font-weight-bold">Tax Id</span>
-                      </v-col>
-                      <v-col cols="3" sm="2">{{
-                        p.invoice.getFromCompanyTaxId()
-                      }}</v-col>
-                      <v-col cols="3" sm="2">
-                        <span class="font-weight-bold">Tel</span>
-                      </v-col>
-                      <v-col cols="3" sm="2">{{
-                        p.invoice.getFromCompanyTel()
-                      }}</v-col>
-                    </v-row>
-                  </p>
-                </v-list-item-subtitle>
-              </v-list-item-content>
+        <v-row justify="center" dense>
+          <h2>{{ title }}</h2>
+        </v-row>
+        <v-row align="stretch" dense>
+          <v-col cols="12">
+            <v-card class="pa-2" outlined>
+              <v-list-item three-line>
+                <v-list-item-content>
+                  <div class="overline mb-4">From Company</div>
+                  <v-list-item-title class="title mb-1">{{
+                    p.invoice.getFromCompanyName()
+                  }}</v-list-item-title>
+                  <v-list-item-subtitle class="text--primary">
+                    <p class="text--primary">
+                      {{ p.invoice.getFromCompanyAddress() }}
+                    </p>
+                    <p>
+                      <v-row dense>
+                        <v-col cols="3" sm="2">
+                          <span class="font-weight-bold">Tax Id</span>
+                        </v-col>
+                        <v-col cols="3" sm="2">{{
+                          p.invoice.getFromCompanyTaxId()
+                        }}</v-col>
+                        <v-col cols="3" sm="2">
+                          <span class="font-weight-bold">Tel</span>
+                        </v-col>
+                        <v-col cols="3" sm="2">{{
+                          p.invoice.getFromCompanyTel()
+                        }}</v-col>
+                      </v-row>
+                    </p>
+                  </v-list-item-subtitle>
+                </v-list-item-content>
 
-              <v-list-item-avatar tile size="130">
-                <v-img
-                  id="from-company-logo"
-                  contain
-                  src="@/assets/logo.png"
-                />
-              </v-list-item-avatar>
-            </v-list-item>
-          </v-card>
-        </v-col>
-      </v-row>
-      <v-row align="stretch" dense>
-        <v-col cols="6">
-          <v-card class="pa-2" outlined>
-            <div class="overline mb-4">To Company</div>
-            <v-card-title class="title">{{
-              p.invoice.getTargetCompanyName()
-            }}</v-card-title>
-            <v-card-text>
-              <p class="text--primary">
-                {{ p.invoice.getTargetCompanyAddress() }}
-              </p>
-              <p class="text--primary">
+                <v-list-item-avatar tile size="130">
+                  <v-img
+                    id="from-company-logo"
+                    contain
+                    src="@/assets/logo.png"
+                  />
+                </v-list-item-avatar>
+              </v-list-item>
+            </v-card>
+          </v-col>
+        </v-row>
+        <v-row align="stretch" dense>
+          <v-col cols="6">
+            <v-card class="pa-2" outlined>
+              <div class="overline mb-4">To Company</div>
+              <v-card-title class="title">{{
+                p.invoice.getTargetCompanyName()
+              }}</v-card-title>
+              <v-card-text>
+                <p class="text--primary">
+                  {{ p.invoice.getTargetCompanyAddress() }}
+                </p>
+                <p class="text--primary">
+                  <v-row dense>
+                    <v-col cols="6" sm="3">
+                      <span class="font-weight-bold">Tax Id</span>
+                    </v-col>
+                    <v-col cols="6" sm="3">{{
+                      p.invoice.getTargetCompanyTaxId()
+                    }}</v-col>
+                    <v-col cols="6" sm="3">
+                      <span class="font-weight-bold">Tel</span>
+                    </v-col>
+                    <v-col cols="6" sm="3">{{
+                      p.invoice.getTargetCompanyTel()
+                    }}</v-col>
+                  </v-row>
+                </p>
+              </v-card-text>
+            </v-card>
+          </v-col>
+          <v-col cols="6">
+            <v-card class="pa-2" outlined>
+              <p class="body-2">
                 <v-row dense>
                   <v-col cols="6" sm="3">
-                    <span class="font-weight-bold">Tax Id</span>
+                    <span class="font-weight-bold">Invoice Number</span>
                   </v-col>
-                  <v-col cols="6" sm="3">{{
-                    p.invoice.getTargetCompanyTaxId()
-                  }}</v-col>
+                  <v-col cols="6" sm="3">{{ p.invoice.invoiceNumber }}</v-col>
                   <v-col cols="6" sm="3">
-                    <span class="font-weight-bold">Tel</span>
+                    <span class="font-weight-bold">Invoice Date</span>
                   </v-col>
-                  <v-col cols="6" sm="3">{{
-                    p.invoice.getTargetCompanyTel()
-                  }}</v-col>
+                  <v-col cols="6" sm="3">{{ p.invoice.invoiceDate }}</v-col>
                 </v-row>
               </p>
-            </v-card-text>
-          </v-card>
-        </v-col>
-        <v-col cols="6">
-          <v-card class="pa-2" outlined>
-            <p class="body-2">
-              <v-row dense>
-                <v-col cols="6" sm="3">
-                  <span class="font-weight-bold">Invoice Number</span>
-                </v-col>
-                <v-col cols="6" sm="3">{{ p.invoice.invoiceNumber }}</v-col>
-                <v-col cols="6" sm="3">
-                  <span class="font-weight-bold">Invoice Date</span>
-                </v-col>
-                <v-col cols="6" sm="3">{{ p.invoice.invoiceDate }}</v-col>
-              </v-row>
-            </p>
-          </v-card>
-        </v-col>
-      </v-row>
-      <v-simple-table>
-        <template v-slot:default>
-          <thead>
-            <tr>
-              <th scope="col" class="text-left">Item</th>
-              <th scope="col" class="text-right">Price</th>
-              <th scope="col" class="text-right">Amount</th>
-              <th scope="col" class="text-right">Total</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr v-for="item in p.invoice.getItems()" :key="item.name">
-              <td class="text-left">{{ item.name }}</td>
-              <td class="text-right">{{ item.getPrice() }}</td>
-              <td class="text-right">{{ item.amount }}</td>
-              <td class="text-right">{{ item.getTotal() }}</td>
-            </tr>
-          </tbody>
-        </template>
-      </v-simple-table>
-      <v-row id="signatures" class="print-only pa-3" dense>
-        <v-col id="receive-signature" cols="6">
-          <v-card outlined>
-            <v-img></v-img>
-            <v-divider></v-divider>
-            <v-card-text>Received By</v-card-text>
-          </v-card>
-        </v-col>
-        <v-col id="approve-signature" cols="6">
-          <v-card outlined>
-            <v-img></v-img>
-            <v-divider></v-divider>
-            <v-card-text>Approved By</v-card-text>
-          </v-card>
-        </v-col>
-      </v-row>
+            </v-card>
+          </v-col>
+        </v-row>
+        <v-simple-table>
+          <template v-slot:default>
+            <thead>
+              <tr>
+                <th scope="col" class="text-left">Item</th>
+                <th scope="col" class="text-right">Price</th>
+                <th scope="col" class="text-right">Amount</th>
+                <th scope="col" class="text-right">Total</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr v-for="item in p.invoice.getItems()" :key="item.name">
+                <td class="text-left">{{ item.name }}</td>
+                <td class="text-right">{{ item.getPrice() }}</td>
+                <td class="text-right">{{ item.amount }}</td>
+                <td class="text-right">{{ item.getTotal() }}</td>
+              </tr>
+            </tbody>
+          </template>
+        </v-simple-table>
+        <v-row id="signatures" class="print-only pa-3" dense>
+          <v-col id="receive-signature" cols="6">
+            <v-card outlined>
+              <v-img></v-img>
+              <v-divider></v-divider>
+              <v-card-text>Received By</v-card-text>
+            </v-card>
+          </v-col>
+          <v-col id="approve-signature" cols="6">
+            <v-card outlined>
+              <v-img></v-img>
+              <v-divider></v-divider>
+              <v-card-text>Approved By</v-card-text>
+            </v-card>
+          </v-col>
+        </v-row>
       </div>
     </div>
     <div class="no-print">
