85880d4593fcd4786fa1aac2d847bf72d3d91bb9
juacompe <juacompe@gmail.com>
2021-01-02 23:09:23 +0700

    format document

--- web/tests/unit/presenters/InvoiceList.spec.js
+++ web/tests/unit/presenters/InvoiceList.spec.js
@@ -1,39 +1,39 @@
-import Presenter from '@/presenters/InvoiceList'
+import Presenter from "@/presenters/InvoiceList";
 
-describe('InvoiceList Presenter', () => {
-  let p = new Presenter()
-  describe('invoices', () => {
-    describe('when there no invoices', () => {
-      it('should be empty list initally or the screen would not render', () => {
-        expect([]).toEqual(p.invoices)
-      })
-    })
-  })
-  describe('headers', () => {
-    it('should contains correct headers', () => {
-      expectHeadersToBeCorrect(p.headers)
-    })
-
-    it('should keep amount out of invoice list page for privacy', () => {
-      p.headers.forEach(header => {
-        expect(Object.values(header)).not.toContain('amount')
+describe("InvoiceList Presenter", () => {
+  let p = new Presenter();
+  describe("invoices", () => {
+    describe("when there no invoices", () => {
+      it("should be empty list initally or the screen would not render", () => {
+        expect([]).toEqual(p.invoices);
       });
-    })
+    });
+  });
+  describe("headers", () => {
+    it("should contains correct headers", () => {
+      expectHeadersToBeCorrect(p.headers);
+    });
+
+    it("should keep amount out of invoice list page for privacy", () => {
+      p.headers.forEach((header) => {
+        expect(Object.values(header)).not.toContain("amount");
+      });
+    });
 
     let expectHeadersToBeCorrect = (headers) => {
-      expect('Number').toEqual(headers[0].text)
-      expect('Company').toEqual(headers[1].text)
-      expect('Project').toEqual(headers[2].text)
-      expect('Date').toEqual(headers[3].text)
-    }
-  })
-  it('should get invoices from API when init', () => {
-    jest.spyOn(p.API, 'getInvoices')
-    p.init(2021)
-    expect(p.API.getInvoices).toHaveBeenCalledWith(2021)
-  })
-  it('should sort by decending invoice number', () => {
-    expect(p.sortBy()).toEqual("invoiceNumber")
-    expect(p.sortDesc()).toEqual(true)
-  })
-})
+      expect("Number").toEqual(headers[0].text);
+      expect("Company").toEqual(headers[1].text);
+      expect("Project").toEqual(headers[2].text);
+      expect("Date").toEqual(headers[3].text);
+    };
+  });
+  it("should get invoices from API when init", () => {
+    jest.spyOn(p.API, "getInvoices");
+    p.init(2021);
+    expect(p.API.getInvoices).toHaveBeenCalledWith(2021);
+  });
+  it("should sort by decending invoice number", () => {
+    expect(p.sortBy()).toEqual("invoiceNumber");
+    expect(p.sortDesc()).toEqual(true);
+  });
+});
