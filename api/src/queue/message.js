a0aaf1096851a578a71796196618c31515b95ba6
juacompe <juacompe@gmail.com>
2021-02-06 12:51:23 +0700

    add guard clause

--- api/src/queue/message.js
+++ api/src/queue/message.js
@@ -3,6 +3,7 @@ function invoiceCreatedMessage(i) {
 }
 
 function total(inv) {
+  if(!inv.items) return 0
   let itemPrices = inv.items.map((i) => i.amount * i.price)
   return itemPrices.reduce((a, b) => a + b)
 }
--- api/tests/unit/queue/message.test.js
+++ api/tests/unit/queue/message.test.js
@@ -2,9 +2,14 @@ const Invoices = require('../../fixtures/invoicesData')
 const invoiceCreatedMessage = require("../../../src/queue/message");
 
 describe("invoice created", () => {
-  it("should have invoice number, company and amount", () => {
+  it("should have invoice number, company and total", () => {
     let inv = Invoices["202001-007"];
     let msg = invoiceCreatedMessage(inv)
     expect(msg).toEqual(`202001-007, Facebook HQ, ${20000 * 20 + 80 * 10}`);
+  });
+  it("should have total 0 for invoice with no items", () => {
+    let inv = Invoices["202001-002"];
+    let msg = invoiceCreatedMessage(inv)
+    expect(msg).toEqual(`202001-002, Googleplex, 0`);
   });
 });
