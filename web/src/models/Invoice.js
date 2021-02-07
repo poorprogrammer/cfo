a71efc9c910ad32a42f4d28da6aca54db582f479
juacompe <juacompe@gmail.com>
2021-02-07 20:09:17 +0700

    prepare for quotations

--- web/src/models/Invoice.js
+++ web/src/models/Invoice.js
@@ -1,96 +1,28 @@
-import InvoiceItem from '@/models/InvoiceItem'
-import PricedInvoiceItem from '@/models/PricedInvoiceItem'
+import PaymentInformation from '@/models/PaymentInformation'
 
-export default class Invoice {
+export default class Invoice extends PaymentInformation {
   constructor(data) {
+    super(data)
     if (!data) return
 
-    this.id = data._id
     this.invoiceNumber = data.invoiceNumber
-    this.companySlug = data.companySlug
     this.invoiceDate = data.invoiceDate
-    this.items = []
-    this.fromCompany = Object.assign({}, data.fromCompany)
-    this.targetCompany = Object.assign({}, data.targetCompany)
-    this.projectName = data.projectName
     this.quotationNumber = data.quotationNumber
     this.purchaseOrderNumber = data.purchaseOrderNumber
-    this.remark = data.remark
-    this.currency = data.currency
-    this.dialog = false
-    this.deleted = data.deleted || false
-    this.currencies = {
-      "THB": new Intl.NumberFormat('th-TH', { style: 'currency', currency: 'THB', currencyDisplay: 'code' }),
-      "USD": new Intl.NumberFormat('us-US', { style: 'currency', currency: 'USD', currencyDisplay: 'code' }),
-    }
-
-    if (!data.items) return
-    data.items.forEach(i => {
-      this.items.push(Object.assign(this.createPricedInvoiceItem(), i))
-    });
   }
   get number() { return this.invoiceNumber }
+  set number(n) { this.invoiceNumber = n }
   get date() { return this.invoiceDate }
-  url() { return `/invoice/${this.invoiceNumber}` }
-  duplicationUrl() { return `/invoice/${this.invoiceNumber}/duplicate` }
-  editionUrl() { return `/invoice/${this.invoiceNumber}/edit` }
-  getItems() { return [...this.items, this.total(), this.tax(), this.grandTotal()] }
-  total() { return new InvoiceItem('Total', this.getTotal(), this) }
-  tax() { return new InvoiceItem('VAT 7%', this.getTotal() * 0.07, this) }
-  grandTotal() { return new InvoiceItem('Grand Total', this.getTotal() * 1.07, this) }
-  getTotal() { return this.items.map(getItemTotal).reduce(sum) }
-  getFromCompanyName() { return this.fromCompany.name }
-  getFromCompanyAddress() { return this.fromCompany.address }
-  getFromCompanyTaxId() { return this.fromCompany.taxId }
-  getFromCompanyTel() { return this.fromCompany.tel }
-  getTargetCompanyName() { return this.targetCompany.name }
-  targetCompanyNameClass() {
-    return this.getTargetCompanyName().length > 40? "small" : ""
-  }
-  itemClass() {
-    return this.items.length > 3? "small": ""
-  }
-  getTargetCompanyAddress() { return this.targetCompany.address }
-  getTargetCompanyTaxId() { return this.targetCompany.taxId }
-  getTargetCompanyTel() { return this.targetCompany.tel }
-  getProjectName() { return this.projectName }
+  set date(d) { this.invoiceDate = d }
+  url() { return `/invoice/${this.number}` }
   getTitles() {
     return [
       {id: 1, title: 'Invoice (original)', css: ''},
       {id: 2, title: 'Invoice (copy)', css: 'print-only'},
     ]
   }
-  getCurrencies() {
-    return Object.keys(this.currencies)
-  }
-  markAsDeleted() {
-    this.deleted = true
-    this.invoiceNumber = `${this.invoiceNumber}-cancelled-${this.currentTimestamp()}`
-  }
-  currentTimestamp() {
-    return new Date().getTime()
-  }
-  addItemBefore(item) {
-    let i = this.items.indexOf(item)
-    if(i<0) i = this.items.length
-    this.items.splice(i, 0, this.createPricedInvoiceItem())
-  }
-  removeItem(item) {
-    let i = this.items.indexOf(item)
-    if(i<0) return
-    this.items.splice(i, 1)
-  }
   setInvoiceDateToday(today=new Date()) {
     this.invoiceDate = this.formatDate(today)
-  }
-  formatDate(date) {
-    let y = date.getFullYear();
-    let m = (1 + date.getMonth()).toString().padStart(2, '0');
-    let d = date.getDate().toString().padStart(2, '0');
-    return `${y}-${m}-${d}`
-  }
-  createPricedInvoiceItem() {
-    return new PricedInvoiceItem(this)
   }
   filename() {
     return `${this.reverseInvoiceNumber()}_INVOICE_${this.targetCompany.name}_${this.projectName}`
@@ -100,6 +32,3 @@ export default class Invoice {
     return `${words[1]}-${words[0].substring(4)}${words[0].substring(0,4)}`
   }
 }
-
-function getItemTotal(item) { return item.total() }
-function sum(x, y) { return x + y }
---
+++ web/src/models/PaymentInformation.js
@@ -0,0 +1,89 @@
+import InvoiceItem from '@/models/InvoiceItem'
+import PricedInvoiceItem from '@/models/PricedInvoiceItem'
+
+export default class PaymentInformation {
+  constructor(data) {
+    if (!data) return
+
+    this.id = data._id
+    this.companySlug = data.companySlug
+    this.items = []
+    this.fromCompany = Object.assign({}, data.fromCompany)
+    this.targetCompany = Object.assign({}, data.targetCompany)
+    this.projectName = data.projectName
+    this.remark = data.remark
+    this.currency = data.currency
+    this.dialog = false
+    this.deleted = data.deleted || false
+    this.currencies = {
+      "THB": new Intl.NumberFormat('th-TH', { style: 'currency', currency: 'THB', currencyDisplay: 'code' }),
+      "USD": new Intl.NumberFormat('us-US', { style: 'currency', currency: 'USD', currencyDisplay: 'code' }),
+    }
+
+    if (!data.items) return
+    data.items.forEach(i => {
+      this.items.push(Object.assign(this.createPricedInvoiceItem(), i))
+    });
+  }
+  url() { return `` }
+  duplicationUrl() { return `${this.url()}/duplicate` }
+  editionUrl() { return `${this.url()}/edit` }
+  getItems() { return [...this.items, this.total(), this.tax(), this.grandTotal()] }
+  total() { return new InvoiceItem('Total', this.getTotal(), this) }
+  tax() { return new InvoiceItem('VAT 7%', this.getTotal() * 0.07, this) }
+  grandTotal() { return new InvoiceItem('Grand Total', this.getTotal() * 1.07, this) }
+  getTotal() { return this.items.map(getItemTotal).reduce(sum) }
+  getFromCompanyName() { return this.fromCompany.name }
+  getFromCompanyAddress() { return this.fromCompany.address }
+  getFromCompanyTaxId() { return this.fromCompany.taxId }
+  getFromCompanyTel() { return this.fromCompany.tel }
+  getTargetCompanyName() { return this.targetCompany.name }
+  targetCompanyNameClass() {
+    return this.getTargetCompanyName().length > 40? "small" : ""
+  }
+  itemClass() {
+    return this.items.length > 3? "small": ""
+  }
+  getTargetCompanyAddress() { return this.targetCompany.address }
+  getTargetCompanyTaxId() { return this.targetCompany.taxId }
+  getTargetCompanyTel() { return this.targetCompany.tel }
+  getProjectName() { return this.projectName }
+  getTitles() {
+    return [
+      {id: 1, title: 'Invoice (original)', css: ''},
+      {id: 2, title: 'Invoice (copy)', css: 'print-only'},
+    ]
+  }
+  getCurrencies() {
+    return Object.keys(this.currencies)
+  }
+  markAsDeleted() {
+    this.deleted = true
+    this.number = `${this.number}-cancelled-${this.currentTimestamp()}`
+  }
+  currentTimestamp() {
+    return new Date().getTime()
+  }
+  addItemBefore(item) {
+    let i = this.items.indexOf(item)
+    if(i<0) i = this.items.length
+    this.items.splice(i, 0, this.createPricedInvoiceItem())
+  }
+  removeItem(item) {
+    let i = this.items.indexOf(item)
+    if(i<0) return
+    this.items.splice(i, 1)
+  }
+  formatDate(date) {
+    let y = date.getFullYear();
+    let m = (1 + date.getMonth()).toString().padStart(2, '0');
+    let d = date.getDate().toString().padStart(2, '0');
+    return `${y}-${m}-${d}`
+  }
+  createPricedInvoiceItem() {
+    return new PricedInvoiceItem(this)
+  }
+}
+
+function getItemTotal(item) { return item.total() }
+function sum(x, y) { return x + y }
