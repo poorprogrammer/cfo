f987c470b1b0d27c8a36f2e61c060198f65b9e00
juacompe <juacompe@gmail.com>
2021-02-13 21:24:23 +0700

    prepare edit quotation

--- web/src/views/DuplicatedInvoice.vue
+++ web/src/views/DuplicatedInvoice.vue
@@ -1,26 +1,14 @@
 <template>
-  <div class="invoice pa-3">
-    <div v-if="!p.invoice.number">Loading Please wait...</div>
-    <div v-else>
-      <edit-invoice v-bind:presenter="p"></edit-invoice>
-      <div class="no-print">
-        <back-button></back-button>
-        <v-btn id="save-button" @click="save" class="mx-1" fab dark color="primary">
-          <v-icon dark>mdi-content-save</v-icon>
-        </v-btn>
-      </div>
-    </div>
-  </div>
+  <duplicated-payment-info v-bind:presenter="p"/>
 </template>
 
 <script>
 import Presenter from '@/presenters/Invoice'
-import EditInvoice from '@/components/EditInvoice.vue'
-import BackButton from '@/components/BackButton.vue'
+import DuplicatedPaymentInfo from '@/components/DuplicatedPaymentInfo.vue'
 import API from '@/services/invoices'
 
 export default {
-  components: { EditInvoice, BackButton },
+  components: { DuplicatedPaymentInfo },
   name: 'duplicatedInvoice',
   mounted() {
     this.p.init(this.$route.params.number)
@@ -31,13 +19,7 @@ export default {
     }
   },
   methods: {
-    save: function() { this.p.save() },
     goTo: function(path) { this.$router.push(path) }
   }
 }
 </script>
-<style scoped>
-a.back-btn {
-  text-decoration: none;
-}
-</style>
