0a726fcc93a578cfd10d44c6201f4c50c5e2a9bf
juacompe <juacompe@gmail.com>
2022-02-24 17:35:22 +0700

    test fail bcoz of .toHaveBeenCalledWith
    ref: https://github.com/vitest-dev/vitest/issues/461

---
+++ web-new/src/services/receipts.js
@@ -0,0 +1,21 @@
+import Receipt from '@/models/Receipt'
+import InvoiceService from '@/services/invoices'
+
+export default class ReceiptService extends InvoiceService {
+  constructor() {
+    super()
+    this.root = process.env.VUE_APP_BASE_API
+  }
+
+  collectionUrl() {
+    return `${this.root}/receipts/`
+  }
+
+  itemUrl(receiptNumber) {
+    return `${this.root}/receipt/${receiptNumber}`
+  }
+
+  createItem = (item) => {
+    return new Receipt(item)
+  }
+}
