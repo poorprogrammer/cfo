04c0a8485b9f8493b13436b56e6d7d1447dcf49a
juacompe <juacompe@gmail.com>
2022-03-17 14:18:36 +0700

    fallback to Vue2
    currently Vuetify3 alpha does not have v-data-table
    it is planned to be released in May.
    
    moving to prime vue or MUI is too much effort.
    I decided to wait for Vuetify 3.
    
    ref:
    - https://vuetifyjs.com/en/introduction/roadmap/
    - https://www.primefaces.org/primevue/\?gclid\=CjwKCAjwlcaRBhBYEiwAK341jTuxTkO75x48xwVWNtuyOkEPuKUgsz1wHEVsL0F6DiHaIR817MSWBhoCpWMQAvD_BwE\#/datatable/sort
    - https://mui.com/components/data-grid/getting-started/
    - https://v2.vuejs.org/v2/guide/installation.html

---
+++ web2/tests/unit/factory.js
@@ -0,0 +1,48 @@
+import Invoice from "../../src/models/Invoice";
+import Receipt from "../../src/models/Receipt";
+
+export class Factory {
+  static json = {
+    amount: 130000,
+    projectName: "React",
+    fromCompany: {
+      name: "ODDS HQ",
+      address: "69 We are not hiring",
+      taxId: "0100000000000",
+      tel: "+66896669999",
+    },
+    targetCompany: {
+      name: "Facebook HQ",
+      address: "1601 Willow Rd Menlo Park, California",
+      taxId: "0100008000007",
+      tel: "+1 650-960-1300",
+    },
+    invoiceDate: "2020-01-03",
+    invoiceNumber: "I202001-001",
+    quotationNumber: "201912-060",
+    purchaseOrderNumber: "PO 20034910343",
+    remark: "Dec 2019",
+    payment: "bank transfer",
+    items: [
+      {
+        name: "Developer",
+        price: 20000,
+        amount: 20,
+      },
+      {
+        name: "Scrum master",
+        price: 80,
+        amount: 10,
+      },
+    ],
+    _id: "oRFlyXTZX9cV6hIS",
+  };
+
+  static createInvoice() {
+    return new Invoice(Factory.json);
+  }
+
+  static createReceipt() {
+    return Receipt.createFromInvoices([this.createInvoice()])
+  }
+}
---
+++ web2/tests/unit/models/quotation.spec.js
@@ -0,0 +1,88 @@
+import Quotation from "../../../src/models/Quotation"
+
+describe('Qutation', () => {
+  let quotation
+  beforeEach(() => {
+    let json = {
+      quotationNumber: "Q202001-001",
+      quotationDate: "2020-01-03",
+      projectName: "React",
+      targetCompany: {
+        name: "Facebook HQ",
+      },
+    }
+    quotation = new Quotation(json)
+  })
+
+  describe('parsing json data', () => {
+    it('should have quotation number', () => {
+      expect(quotation.quotationNumber).toEqual('Q202001-001')
+    })
+
+    it('should not have receipt number', () => {
+      expect(quotation.hasReceiptNumber).toEqual(false)
+    })
+
+    it('should have duplicattion url', () => {
+      expect(quotation.duplicationUrl()).toEqual('/quotation/Q202001-001/duplicate')
+    })
+  })
+
+  describe('Printing', () => {
+    it('should have original', () => {
+      let titles = quotation.getTitles();
+      expect(titles[0].title).toEqual('Quotation (original)')
+    })
+    it('should have copy', () => {
+      let titles = quotation.getTitles();
+      expect(titles[1].title).toEqual('Quotation (copy)')
+    })
+    it('should have filename to be save when export to PDF and store in storage', () => {
+      expect(quotation.filename()).toEqual('Q202001-001_QUOTATION_Facebook HQ_React')
+    })
+    it('should always have normal signature class as it does not have payment info like invoice', () => {
+      expect(quotation.tablePaddingClass()).toEqual('')
+    })
+  })
+
+  describe('list quotation', () => {
+    it('should have number expeced by presenter', () => {
+      expect(quotation.number).toEqual('Q202001-001')
+    })
+    it('should have date expeced by presenter', () => {
+      expect(quotation.date).toEqual('2020-01-03')
+    })
+  })
+
+  describe('duplicate quotation', () => {
+    it('should be able to set quotation date to today', () => {
+      let today = new Date("January 13, 2021")
+      quotation.quotationDate = '2021-01-02'
+      quotation.setDateToday(today)
+      expect(quotation.quotationDate).toEqual('2021-01-13')
+    })
+    it('should not show invoice number, invoice date and PO field to edit', () => {
+      expect(quotation.hasInvoiceNumber).toEqual(false)
+    })
+  })
+
+  describe('update quotation', () => {
+    it('should have listUrl to redirect to after saved', () => {
+      expect(quotation.listUrl.name).toEqual('quotations')
+    })
+  })
+
+  describe("delete quotation", () => {
+    it('should update quotation number so the quotation number can be reused while unique', () => {
+      let t = '1610194022999'
+      mockCurrentTimestamp(quotation, t)
+
+      quotation.markAsDeleted()
+
+      expect(quotation.quotationNumber).toEqual(`Q202001-001-cancelled-${t}`)
+    })
+  })
+})
+function mockCurrentTimestamp(quotation, timestamp) {
+  jest.spyOn(quotation, 'currentTimestamp').mockReturnValue(timestamp)
+}
