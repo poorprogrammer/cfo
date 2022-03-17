382c5830905ad955fd42cdab906930e40de11c75
juacompe <juacompe@gmail.com>
2022-03-17 22:13:31 +0700

    move e2e tests

---
+++ web2/tests/e2e/specs/invoices.js
@@ -0,0 +1,34 @@
+describe('List Invoices Page', () => {
+  beforeEach(() => {
+    cy.visit('/invoices/2020')
+    cy.login()
+  })
+
+  it('Prints an old invoice', () => {
+    cy.visit('/invoices/2020')
+    shouldSeeLastestInvoiceNumber(cy)
+    clickPrintInvoice(cy, '202001-001')
+    shouldSee(cy, 'Training')
+  })
+
+  it('Should be able to get back to home page from whereever page by clicking at logo', () => {
+    cy.visit('/shouldNotHaveThisPage')
+    cy.contains('Page not found')
+    cy.get('#app_logo').click()
+    cy.contains('Invoice List')
+  })
+
+  function shouldSeeLastestInvoiceNumber(cy) {
+    let latestInvoiceNumber = '202001-007'
+    shouldSee(cy, latestInvoiceNumber)
+  }
+
+  function clickPrintInvoice(cy, invoiceNumber) {
+    cy.get(`#print_${invoiceNumber}`).click()
+  }
+
+  function shouldSee(cy, expected) {
+    cy.contains(expected)
+  }
+
+})
--- web2/tests/e2e/support/commands.js
+++ web2/tests/e2e/support/commands.js
@@ -1,25 +1,37 @@
-// ***********************************************
-// This example commands.js shows you how to
-// create various custom commands and overwrite
-// existing commands.
-//
-// For more comprehensive examples of custom
-// commands please read more here:
-// https://on.cypress.io/custom-commands
-// ***********************************************
-//
-//
-// -- This is a parent command --
-// Cypress.Commands.add("login", (email, password) => { ... })
-//
-//
-// -- This is a child command --
-// Cypress.Commands.add("drag", { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add("dismiss", { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This is will overwrite an existing command --
-// Cypress.Commands.overwrite("visit", (originalFn, url, options) => { ... })
+Cypress.Commands.add("containsOriginalInvoiceHeading", () => {
+    cy.contains('Invoice (original)')
+})
+
+Cypress.Commands.add("containsInvoiceNumber", (invoiceNumber) => {
+    cy.contains('Invoice Number')
+    cy.contains(invoiceNumber)
+})
+
+
+Cypress.Commands.add("containsItemNameOnRow", (row, value) => {
+    cy.get(`div.v-data-table tr:nth-child(${row}) td:nth-child(1)`)
+      .contains(value)
+})
+
+Cypress.Commands.add("containsTotal", (total) => {
+    cy.get('div.v-data-table tr:last-child td:last-child')
+      .contains(total)
+})
+
+Cypress.Commands.add("login", (username, password) => {
+    cy.server()
+    cy.route({
+        method: 'POST',
+        url: '/login/',
+    }).as('login')
+    username = username || "user"
+    password = password || "s3cr3t"
+    cy.input("#username", username)
+    cy.input("#password", password)
+    cy.get("#login-button").click()
+    cy.wait('@login')
+})
+
+Cypress.Commands.add("input", (key, value) => {
+    cy.get(key).click().type('{selectall}'+value)
+})
