6a547e47ad401da42478d436d61d6df02c112841
juacompe <juacompe@gmail.com>
2022-09-29 13:18:31 +0700

    fix failing test

--- api/src/persistence/nedb.js
+++ api/src/persistence/nedb.js
@@ -1,48 +1,47 @@
-const Datastore = require("nedb")
-const config = require("config")
+const Datastore = require('nedb')
+const config = require('config')
 
 module.exports = class Database {
-    constructor(collection) {
-        this.db = new Datastore({ filename: `${config.get("DB_PATH")}/${collection}s`, autoload: true })
-        this.db.ensureIndex({ fieldName: `${collection}Number`, unique: true }, function (err) {
-            if(err) console.log(err)
-        });
+  constructor (collection) {
+    this.db = new Datastore({ filename: `${config.get('DB_PATH')}/${collection}s`, autoload: true })
+    this.db.ensureIndex({ fieldName: `${collection}Number`, unique: true }, function (err) {
+      if (err) console.log(err)
+    })
+  }
 
-    }
+  insert (data) {
+    return new Promise((resolve, reject) => {
+      this.db.insert(data, (e, docs) => {
+        if (e != null) reject(e)
+        resolve(docs)
+      })
+    })
+  }
 
-    insert(data) {
-        return new Promise((resolve, reject) => {
-            this.db.insert(data, (e, docs) => {
-                if (e != null) reject(e)
-                resolve(docs)
-            })
-        })
-    }
+  update (id, data) {
+    return new Promise((resolve, reject) => {
+      this.db.update(id, data, {}, (e, n, docs) => {
+        if (e != null || n === 0) reject(e)
+        resolve(docs)
+      })
+    })
+  }
 
-    update(id, data) {
-        return new Promise((resolve, reject) => {
-            this.db.update(id, data, {}, (e, n, docs) => {
-                if (e != null || n === 0) reject(e)
-                resolve(docs)
-            })
-        })
-    }
+  find (query) {
+    return new Promise((resolve, reject) => {
+      this.db.find(query, (e, docs) => {
+        if (e != null) reject(e)
+        resolve(docs)
+      })
+    })
+  }
 
-    find(query) {
-        return new Promise((resolve, reject) => {
-            this.db.find(query, (e, docs) => {
-                if (e != null) reject(e)
-                resolve(docs)
-            })
-        })
-    }
-
-    findOne(query) {
-        return new Promise((resolve, reject) => {
-            this.db.findOne(query, (e, doc) => {
-                if (e != null) reject(e)
-                resolve(doc)
-            })
-        })
-    }
+  findOne (query) {
+    return new Promise((resolve, reject) => {
+      this.db.findOne(query, (e, doc) => {
+        if (e != null) reject(e)
+        resolve(doc)
+      })
+    })
+  }
 }
