bd61789a0f236e99c07d878642425df60356a710
juacompe <juacompe@gmail.com>
2022-09-29 13:18:05 +0700

    fix failing test

--- api/src/queue/message.js
+++ api/src/queue/message.js
@@ -1,36 +1,35 @@
-function invoiceCreatedMessage(i) {
-  return `I${i.invoiceNumber}| ${targetCompanyName(i)}| ${toCurrency(total(i), i.currency)}`;
+function invoiceCreatedMessage (i) {
+  return `I${i.invoiceNumber}| ${targetCompanyName(i)}| ${toCurrency(total(i), i.currency)}`
 }
 
 function quotationCreatedMessage (i) {
-  return `Q${i.quotationNumber}| ${targetCompanyName(i)}| ${toCurrency(total(i), i.currency)}`;
+  return `Q${i.quotationNumber}| ${targetCompanyName(i)}| ${toCurrency(total(i), i.currency)}`
 }
 
 function receiptCreatedMessage (i) {
-  return `R${i.receiptNumber}| ${targetCompanyName(i)}| ${toCurrency(total(i), i.currency)}`;
+  return `R${i.receiptNumber}| ${targetCompanyName(i)}| ${toCurrency(total(i), i.currency)}`
 }
 
-function targetCompanyName(i) {
-  return i.targetCompany? i.targetCompany.name: "";
+function targetCompanyName (i) {
+  return i.targetCompany ? i.targetCompany.name : ''
 }
 
-function total(inv) {
-  if(!inv.items) return 0
-  let itemPrices = inv.items.map((i) => i.amount * i.price)
+function total (inv) {
+  if (!inv.items) return 0
+  const itemPrices = inv.items.map((i) => i.amount * i.price)
   return itemPrices.reduce((a, b) => a + b, 0)
 }
 
-function toCurrency(amount, currency='THB') {
+function toCurrency (amount, currency = 'THB') {
   const currencies = {
-    "THB": new Intl.NumberFormat('th-TH', { style: 'currency', currency: 'THB', currencyDisplay: 'code' }),
-    "USD": new Intl.NumberFormat('us-US', { style: 'currency', currency: 'USD', currencyDisplay: 'code' }),
+    THB: new Intl.NumberFormat('th-TH', { style: 'currency', currency: 'THB', currencyDisplay: 'code' }),
+    USD: new Intl.NumberFormat('us-US', { style: 'currency', currency: 'USD', currencyDisplay: 'code' })
   }
   return currencies[currency].format(amount)
-
 }
 
 module.exports = {
   invoiceCreatedMessage,
   quotationCreatedMessage,
-  receiptCreatedMessage,
-};
+  receiptCreatedMessage
+}
