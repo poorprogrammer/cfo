03f3977a03fddde06cc675bacbd0b1846ed642fd
juacompe <juacompe@gmail.com>
2022-09-29 12:59:32 +0700

    fix lint

--- api/tests/unit/queue/message.test.js
+++ api/tests/unit/queue/message.test.js
@@ -1,35 +1,35 @@
 const Invoices = require('../../fixtures/invoicesData')
 const Quotations = require('../../fixtures/quotationsData')
-const m = require("../../../src/queue/message");
+const m = require('../../../src/queue/message')
 
-describe("invoice created", () => {
-  it("should have invoice number, company and total", () => {
-    let inv = Invoices["202001-007"];
-    let msg = m.invoiceCreatedMessage(inv)
-    expect(msg).toEqual(`I202001-007| Facebook HQ| THB 400,800.00`);
-  });
-  it("should have total 0 for invoice with no items", () => {
-    let inv = Invoices["202001-002"];
-    let msg = m.invoiceCreatedMessage(inv)
-    expect(msg).toEqual(`I202001-002| Googleplex| THB 0.00`);
-  });
-  it("should have total 0 for invoice with no items (empty arrary)", () => {
-    let inv = Invoices["202001-002"];
+describe('invoice created', () => {
+  it('should have invoice number, company and total', () => {
+    const inv = Invoices['202001-007']
+    const msg = m.invoiceCreatedMessage(inv)
+    expect(msg).toEqual('I202001-007| Facebook HQ| THB 400,800.00')
+  })
+  it('should have total 0 for invoice with no items', () => {
+    const inv = Invoices['202001-002']
+    const msg = m.invoiceCreatedMessage(inv)
+    expect(msg).toEqual('I202001-002| Googleplex| THB 0.00')
+  })
+  it('should have total 0 for invoice with no items (empty arrary)', () => {
+    const inv = Invoices['202001-002']
     inv.items = []
-    let msg = m.invoiceCreatedMessage(inv)
-    expect(msg).toEqual(`I202001-002| Googleplex| THB 0.00`);
-  });
-  it("should support USD", () => {
-    let inv = Invoices["202001-001"];
-    let msg = m.invoiceCreatedMessage(inv)
-    expect(msg).toEqual(`I202001-001| SPACEX HEADQUARTERS| USD 14,950.00`);
-  });
-});
+    const msg = m.invoiceCreatedMessage(inv)
+    expect(msg).toEqual('I202001-002| Googleplex| THB 0.00')
+  })
+  it('should support USD', () => {
+    const inv = Invoices['202001-001']
+    const msg = m.invoiceCreatedMessage(inv)
+    expect(msg).toEqual('I202001-001| SPACEX HEADQUARTERS| USD 14,950.00')
+  })
+})
 
-describe("quotation created", () => {
-  it("should have quotation number, company and total", () => {
-    let q = Quotations["202001-001"];
-    let msg = m.quotationCreatedMessage(q)
-    expect(msg).toEqual(`Q202001-001| SPACEX HEADQUARTERS| USD 14,950.00`);
-  });
-});
+describe('quotation created', () => {
+  it('should have quotation number, company and total', () => {
+    const q = Quotations['202001-001']
+    const msg = m.quotationCreatedMessage(q)
+    expect(msg).toEqual('Q202001-001| SPACEX HEADQUARTERS| USD 14,950.00')
+  })
+})
