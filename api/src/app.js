24340f6b7f62c5a14ff0d571b7d94a00aedd48ba
juacompe <juacompe@gmail.com>
2022-09-29 08:54:10 +0700

    try to fix code smell on sonar
    install eslint using javascript standard
    fix app.js and server.js

--- api/src/app.js
+++ api/src/app.js
@@ -1,118 +1,118 @@
-const express = require("express");
-const bodyParser = require("body-parser");
-const app = express();
-const cors = require("cors");
-const Invoice = require("./services/invoice");
-const Quotation = require("./services/quotation");
-const Receipt = require("./services/receipt");
-const Auth = require("./services/auth");
+const express = require('express')
+const bodyParser = require('body-parser')
+const app = express()
+const cors = require('cors')
+const Invoice = require('./services/invoice')
+const Quotation = require('./services/quotation')
+const Receipt = require('./services/receipt')
+const Auth = require('./services/auth')
 
-app.disable("x-powered-by");
-app.use(cors());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
+app.disable('x-powered-by')
+app.use(cors())
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
 
-app.get("/", (req, res) => res.send("Hello World!"));
-app.get("/invoices/:year", (req, res) => {
+app.get('/', (_req, res) => res.send('Hello World!'))
+app.get('/invoices/:year', (req, res) => {
   new Invoice().all(req.params.year).then((invoices) => {
-    res.json(invoices);
-  });
-});
-app.get("/invoice/:invoiceNumber", function (req, res) {
+    res.json(invoices)
+  })
+})
+app.get('/invoice/:invoiceNumber', function (req, res) {
   new Invoice().get(req.params.invoiceNumber).then((inv) => {
-    res.json(inv);
-  });
-});
-app.put("/invoice/:invoiceNumber/", function (req, res) {
+    res.json(inv)
+  })
+})
+app.put('/invoice/:invoiceNumber/', function (req, res) {
   new Invoice()
     .update(req.body)
-    .then((inv) => {
-      res.json(req.body);
+    .then((_inv) => {
+      res.json(req.body)
     })
-    .catch(serverErrorHandler(res));
-});
-app.post("/invoices/", function (req, res) {
+    .catch(serverErrorHandler(res))
+})
+app.post('/invoices/', function (req, res) {
   new Invoice()
     .save(req.body)
     .then((id) => {
-      res.json(id);
+      res.json(id)
     })
-    .catch(serverErrorHandler(res));
-});
-app.get("/quotations/:year", (req, res) => {
+    .catch(serverErrorHandler(res))
+})
+app.get('/quotations/:year', (req, res) => {
   new Quotation().all(req.params.year).then((quotations) => {
-    res.json(quotations);
-  });
-});
-app.get("/quotation/:number", function (req, res) {
+    res.json(quotations)
+  })
+})
+app.get('/quotation/:number', function (req, res) {
   new Quotation().get(req.params.number).then((doc) => {
-    res.json(doc);
-  });
-});
-app.post("/quotations/", function (req, res) {
+    res.json(doc)
+  })
+})
+app.post('/quotations/', function (req, res) {
   new Quotation()
     .save(req.body)
     .then((id) => {
-      res.json(id);
+      res.json(id)
     })
-    .catch(serverErrorHandler(res));
-});
-app.put("/quotation/:number", function (req, res) {
+    .catch(serverErrorHandler(res))
+})
+app.put('/quotation/:number', function (req, res) {
   new Quotation()
     .update(req.body)
-    .then((inv) => {
-      res.json(req.body);
+    .then((_inv) => {
+      res.json(req.body)
     })
-    .catch(serverErrorHandler(res));
-});
-app.get("/receipts/:year", (req, res) => {
+    .catch(serverErrorHandler(res))
+})
+app.get('/receipts/:year', (req, res) => {
   new Receipt().all(req.params.year).then((invoices) => {
-    res.json(invoices);
-  });
-});
-app.get("/receipt/:number", function (req, res) {
+    res.json(invoices)
+  })
+})
+app.get('/receipt/:number', function (req, res) {
   new Receipt().get(req.params.number).then((doc) => {
-    res.json(doc);
-  });
-});
-app.post("/receipts/", function (req, res) {
+    res.json(doc)
+  })
+})
+app.post('/receipts/', function (req, res) {
   new Receipt()
     .save(req.body)
     .then((id) => {
-      res.json(id);
+      res.json(id)
     })
-    .catch(serverErrorHandler(res));
-});
-app.put("/receipt/:number", function (req, res) {
+    .catch(serverErrorHandler(res))
+})
+app.put('/receipt/:number', function (req, res) {
   new Receipt()
     .update(req.body)
-    .then((inv) => {
-      res.json(req.body);
+    .then((_inv) => {
+      res.json(req.body)
     })
-    .catch(serverErrorHandler(res));
-});
-app.post("/login/", function (req, res) {
+    .catch(serverErrorHandler(res))
+})
+app.post('/login/', function (req, res) {
   new Auth()
     .login(req.body.password)
     .then((out) => {
-      res.json(out);
+      res.json(out)
     })
-    .catch(serverErrorHandler(res));
-});
+    .catch(serverErrorHandler(res))
+})
 
-let serverErrorHandler = (res) => {
+const serverErrorHandler = (res) => {
   return (error) => {
     try {
       if (error.status === 401) {
-        res.status(error.status);
-        res.json(error.message);
+        res.status(error.status)
+        res.json(error.message)
       }
-      throw error;
+      throw error
     } catch (e) {
-      console.log(e);
-      res.status(500);
-      res.json({ error: e.toString() });
+      console.log(e)
+      res.status(500)
+      res.json({ error: e.toString() })
     }
-  };
-};
-module.exports = app;
+  }
+}
+module.exports = app
--- api/src/server.js
+++ api/src/server.js
@@ -1,6 +1,6 @@
-const http = require('http');
-const app = require('./app');
-const server = http.createServer(app);
-const port = process.env.PORT || 3000;
+const http = require('http')
+const app = require('./app')
+const server = http.createServer(app)
+const port = process.env.PORT || 3000
 
 server.listen(port, () => console.log(`Started app listening on port ${port}!`))
