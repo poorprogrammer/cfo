5ef9a11edf1fbacfe6bcf3c32023f05f01474e61
Phongsak Ritpitakphong <jane.makub@gmail.com>
2022-12-11 20:24:22 +0700

    remove eslint plugin

--- web/.eslintrc.js
+++ web/.eslintrc.js
@@ -6,7 +6,7 @@ module.exports = {
   extends: [
     "plugin:vue/essential",
     "eslint:recommended",
-    "plugin:prettier/recommended",
+
   ],
   parserOptions: {
     parser: "@babel/eslint-parser",
