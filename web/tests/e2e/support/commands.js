96e70c8b2040be664262053370ba5eb06132bd82
juacompe <juacompe@gmail.com>
2024-05-03 17:52:59 +0700

    remove unused commands

--- web/tests/e2e/support/commands.js
+++ web/tests/e2e/support/commands.js
@@ -6,19 +6,3 @@ Cypress.Commands.add("containsInvoiceNumber", (invoiceNumber) => {
   cy.contains("Invoice Number");
   cy.contains(invoiceNumber);
 });
-
-Cypress.Commands.add("containsItemNameOnRow", (row, value) => {
-  cy.get(`div.v-data-table tr:nth-child(${row}) td:nth-child(1)`).contains(
-    value
-  );
-});
-
-Cypress.Commands.add("containsTotal", (total) => {
-  cy.get("div.v-data-table tr:last-child td:last-child").contains(total);
-});
-
-Cypress.Commands.add("input", (key, value) => {
-  cy.get(key)
-    .click()
-    .type("{selectall}" + value);
-});
