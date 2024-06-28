840f6895d49be593dd6a56ee72a149a4d061c1c4
Phongsak Ritpitakphong <jane.makub@gmail.com>
2024-06-28 17:57:43 +0700

    Remove Playwright and Cypress from Web

--- web/tests/e2e/support/commands.js
+++
@@ -1,8 +0,0 @@
-Cypress.Commands.add("containsOriginalInvoiceHeading", () => {
-  cy.contains("Invoice (original)");
-});
-
-Cypress.Commands.add("containsInvoiceNumber", (invoiceNumber) => {
-  cy.contains("Invoice Number");
-  cy.contains(invoiceNumber);
-});
