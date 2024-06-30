29582813bc0b21474d77138a2ea4ba29a68c81e2
Phongsak Ritpitakphong <jane.makub@gmail.com>
2024-06-30 20:28:42 +0700

    Refactor to use common component

--- web/src/views/Login.vue
+++ web/src/views/Login.vue
@@ -3,14 +3,8 @@
     <v-col cols="6">
       <v-card outlined class="pa-2">
         <div>
-          <v-text-field id="username" label="Username" v-model="p.username" />
-          <v-text-field
-            id="password"
-            label="Password"
-            type="password"
-            v-model="p.password"
-          />
-
+          <base-input id="username" label="Username" v-model="p.username"/>
+          <base-input id="password" label="Password" v-model="p.password" type="password"/>
           <v-btn
             id="login-button"
             class="mx-1"
@@ -29,9 +23,11 @@
 
 <script>
 import Presenter from "@/presenters/Login";
+import BaseInput from "@/components/BaseInput.vue";
 
 export default {
   name: "LoginView",
+  components: { BaseInput },
   data() {
     return {
       p: new Presenter(this),
