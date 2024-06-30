d0187b323310adf7f2dfd95490d6b843ba95c969
Phongsak Ritpitakphong <jane.makub@gmail.com>
2024-06-30 20:29:44 +0700

    Add script to run test in local

--- e2e/playwright.config.js
+++ e2e/playwright.config.js
@@ -27,8 +27,8 @@ module.exports = defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
-    baseURL: 'http://web:8080',
-
+    baseURL: process.env.DEV ? 'http://localhost:8080' : 'http://web:8080',
+    // baseURL: 'http://localhost:8080',
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
   },
