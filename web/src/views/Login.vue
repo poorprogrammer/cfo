f7c65f7442560228c8226b136bc1fe03b05da4f6
juacompe <juacompe@gmail.com>
2024-11-28 16:14:47 +0700

    remove v-icon

--- web/src/views/Login.vue
+++ web/src/views/Login.vue
@@ -3,18 +3,17 @@
     <v-col cols="6">
       <v-card outlined class="pa-2">
         <div>
-          <base-input id="username" label="Username" v-model="p.username"/>
-          <base-input id="password" label="Password" v-model="p.password" type="password"/>
-          <v-btn
-            id="login-button"
-            class="mx-1"
-            dark
-            color="primary"
-            @click="login"
-          >
-            Login
-            <v-icon dark>mdi-login</v-icon>
-          </v-btn>
+          <base-input id="username" label="Username" v-model="p.username" />
+          <base-input
+            id="password"
+            label="Password"
+            v-model="p.password"
+            type="password"
+          />
+          <button id="login-button" class="login-button" @click="login">
+            <span class="button-text">Login</span>
+            <span class="icon">➜</span>
+          </button>
         </div>
       </v-card>
     </v-col>
@@ -43,3 +42,32 @@ export default {
   },
 };
 </script>
+
+<style scoped>
+.login-button {
+  display: flex;
+  align-items: center;
+  gap: 8px;
+  padding: 8px 16px;
+  margin: 4px;
+  background-color: var(--primary-color, #1976d2);
+  color: white;
+  border: none;
+  border-radius: 4px;
+  cursor: pointer;
+  font-size: 1em;
+  transition: background-color 0.2s;
+}
+
+.login-button:hover {
+  background-color: var(--primary-hover-color, #1565c0);
+}
+
+.login-button .icon {
+  font-size: 1.2em;
+}
+
+.button-text {
+  font-weight: 500;
+}
+</style>
