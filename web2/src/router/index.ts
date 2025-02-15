b3bf3538fd75418fb229585df6b0b102420929fa
juacompe <juacompe@gmail.com>
2025-02-15 11:01:45 +0700

    require Login before accessing invoices page

--- web2/src/router/index.ts
+++ web2/src/router/index.ts
@@ -1,45 +1,70 @@
-import { createRouter, createWebHistory, RouteRecordRaw } from "vue-router";
+import {
+  createRouter,
+  createWebHistory,
+  NavigationGuardNext,
+  RouteLocationNormalized,
+  RouteRecordRaw,
+} from "vue-router";
 import Invoices from "@/views/Invoices.vue";
 import DuplicatedInvoice from "@/views/DuplicatedInvoice.vue";
 import UpdateInvoice from "@/views/UpdateInvoice.vue";
 import Invoice from "@/views/Invoice.vue";
+import Login from "@/views/Login.vue";
+
+const isAuthenticated = (
+  to: RouteLocationNormalized,
+  from: RouteLocationNormalized,
+  next: NavigationGuardNext
+) => {
+  console.log("enter isAuthenticated");
+  if (localStorage.getItem("token")) {
+    next();
+    return;
+  }
+  next({ name: "login" });
+};
 
 const routes: Array<RouteRecordRaw> = [
+  {
+    path: "/login",
+    name: "login",
+    component: Login,
+  },
   {
     path: "/",
     name: "home",
     redirect: { name: "invoicesThisYear" },
-    // beforeEnter: isAuthenticated,
+    beforeEnter: isAuthenticated,
   },
   {
     path: "/invoices",
     name: "invoicesThisYear",
     component: Invoices,
-    // beforeEnter: isAuthenticated,
+    beforeEnter: isAuthenticated,
   },
   {
     path: "/invoices/:year",
     name: "invoices",
     component: Invoices,
-    // beforeEnter: isAuthenticated,
+    beforeEnter: isAuthenticated,
   },
   {
     path: "/invoice/:number/duplicate",
     name: "duplicatedInvoice",
     component: DuplicatedInvoice,
-    // beforeEnter: isAuthenticated,
+    beforeEnter: isAuthenticated,
   },
   {
     path: "/invoice/:number/edit",
     name: "updateInvoice",
     component: UpdateInvoice,
-    // beforeEnter: isAuthenticated,
+    beforeEnter: isAuthenticated,
   },
   {
     path: "/invoice/:number",
     name: "invoice",
     component: Invoice,
-    // beforeEnter: isAuthenticated,
+    beforeEnter: isAuthenticated,
   },
   {
     path: "/quotations",
