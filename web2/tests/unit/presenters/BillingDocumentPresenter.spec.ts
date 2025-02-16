7591c49a29fd1eab79c152261333622d3c78e7d3
juacompe <juacompe@gmail.com>
2025-02-16 20:59:59 +0700

    convert to ts

--- web2/tests/unit/presenters/BillingDocumentPresenter.spec.ts
+++ web2/tests/unit/presenters/BillingDocumentPresenter.spec.ts
@@ -1,5 +1,7 @@
+import BillingDocument from "@/models/BillingDocument";
 import Presenter from "@/presenters/BillingDocumentPresenter";
 import API from "@/services/InvoiceService";
+import { Mock, vi } from "vitest";
 
 export class MockView {
   goTo() {
@@ -8,28 +10,30 @@ export class MockView {
 }
 
 describe("Invoice Presenter", () => {
-  let invoiceNumber, p;
+  let invoiceNumber: string;
+  let p: Presenter;
+
   beforeEach(() => {
     p = new Presenter(new MockView(), new API());
     invoiceNumber = "202001-001";
   });
 
   it("should get invoice with specific number from API when init", () => {
-    vi.spyOn(p.API, "get").mockResolvedValue({ data: {} });
+    vi.spyOn(p.API, "get").mockResolvedValue({} as BillingDocument);
     p.init(invoiceNumber);
-    expectToBeCalledWith(p.API, invoiceNumber);
+    expectToBeCalledWith(p.API.get as Mock, invoiceNumber);
   });
   it("should save the current invoice", () => {
     const invoiceNumber = "202001-001";
     vi.spyOn(p.API, "save").mockResolvedValueOnce("ok");
-    p.billingDocument.value.invoiceNumber = invoiceNumber;
+    p.billingDocument.value.number = invoiceNumber;
     p.save();
     expect(p.API.save).toHaveBeenCalledWith(p.billingDocument.value);
   });
   it("should redirect to view page after save new invoice so it is ready to be printed", async () => {
     givenSaveSuccessWithInvoiceNumber(invoiceNumber);
     await p.save();
-    expectToRedirectToViewInvoicePage(p.view, invoiceNumber);
+    expectToRedirectToViewInvoicePage(p.view as MockView, invoiceNumber);
   });
   it("should show error after save duplicated invoice", async () => {
     givenSaveFailedWithError("Error: Request failed with status code 500");
@@ -39,31 +43,36 @@ describe("Invoice Presenter", () => {
   it("should redirect to view page after update existing invoice so it is ready to be printed", async () => {
     givenUpdateSuccessWithInvoiceNumber(invoiceNumber);
     await p.update();
-    expectToRedirectToViewInvoicePage(p.view, invoiceNumber);
+    expectToRedirectToViewInvoicePage(p.view as MockView, invoiceNumber);
   });
-  function givenSaveSuccessWithInvoiceNumber(invoiceNumber) {
+  function givenSaveSuccessWithInvoiceNumber(invoiceNumber: string) {
     vi.spyOn(p.view, "goTo");
     vi.spyOn(p.API, "save").mockResolvedValue(invoiceNumber);
   }
-  function givenUpdateSuccessWithInvoiceNumber(invoiceNumber) {
+  function givenUpdateSuccessWithInvoiceNumber(invoiceNumber: string) {
     vi.spyOn(p.view, "goTo");
-    vi.spyOn(p.API, "update").mockResolvedValue({ number: invoiceNumber });
+    vi.spyOn(p.API, "update").mockResolvedValue({
+      number: invoiceNumber,
+    } as BillingDocument);
   }
-  function givenSaveFailedWithError(err) {
-    vi.spyOn(p, "showError").mockImplementation();
+  function givenSaveFailedWithError(err: string) {
+    vi.spyOn(p, "showError").mockImplementation(vi.fn());
     vi.spyOn(p.API, "save").mockRejectedValue(err);
   }
-  function expectToBeCalledWith(api, invoiceNumber) {
-    expect(api.get).toHaveBeenCalled();
-    expect(api.get.mock.calls[0][0]).toEqual(invoiceNumber);
+  function expectToBeCalledWith(get: Mock, invoiceNumber: string) {
+    expect(get).toHaveBeenCalled();
+    expect(get.mock.calls[0][0]).toEqual(invoiceNumber);
   }
-  function expectToRedirectToViewInvoicePage(view, invoiceNumber) {
+  function expectToRedirectToViewInvoicePage(
+    view: MockView,
+    invoiceNumber: string
+  ) {
     expect(view.goTo).toHaveBeenCalledWith({
       name: "invoice",
       params: { number: invoiceNumber },
     });
   }
-  function expectPopupShownWithError(error) {
+  function expectPopupShownWithError(error: string) {
     expect(p.showError).toHaveBeenCalledWith(error);
   }
 });
