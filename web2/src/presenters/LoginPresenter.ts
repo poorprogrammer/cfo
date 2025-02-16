95c3aa51a37715bc6bd7e761437113c98d07f8e3
juacompe <juacompe@gmail.com>
2025-02-16 14:14:39 +0700

    fix type

--- web2/src/presenters/LoginPresenter.ts
+++ web2/src/presenters/LoginPresenter.ts
@@ -1,21 +1,29 @@
 import API from "@/services/LoginService";
-export default class {
-  constructor(view) {
+import { View } from "./BillingArchivePresenter";
+
+export default class LoginPresenter {
+  view: View;
+  API: API;
+  username: string;
+  password: string;
+  constructor(view: View) {
     this.view = view;
     this.API = new API();
+    this.username = "";
+    this.password = "";
   }
   login() {
     this.API.login(this.username, this.password).then(
-      (token) => {
+      (token: string) => {
         localStorage.setItem("token", token);
-        this.view.goTo({ name: "home" });
+        this.view.goTo({ name: "home", params: {} });
       },
-      (error) => {
+      (error: string) => {
         this.showError(error);
       }
     );
   }
-  showError(error) {
+  showError(error: string) {
     alert(error);
   }
 }
