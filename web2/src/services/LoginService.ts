75aff501704d5c52e254f0467c49339dd982ef41
juacompe <juacompe@gmail.com>
2025-02-16 09:56:21 +0700

    convert to ts

--- web2/src/services/LoginService.ts
+++ web2/src/services/LoginService.ts
@@ -1,11 +1,13 @@
 import axios from "axios";
 
 export default class LoginService {
+  protected root: string;
+
   constructor() {
-    this.root = process.env.VUE_APP_BASE_API;
+    this.root = process.env.VUE_APP_BASE_API || "";
   }
 
-  login(username, password) {
+  login(username: string, password: string) {
     return axios.post(`${this.root}/login/`, {
       username: username,
       password: password,
