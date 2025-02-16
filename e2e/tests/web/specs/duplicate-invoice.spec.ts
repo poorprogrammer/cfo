ef8bc1c920d07fc127a3add60bc8b85efbe1786d
juacompe <juacompe@gmail.com>
2025-02-16 23:04:12 +0700

    refactor: specify type

--- e2e/tests/web/specs/duplicate-invoice.spec.ts
+++ e2e/tests/web/specs/duplicate-invoice.spec.ts
@@ -1,9 +1,10 @@
 import { test } from "@playwright/test";
 import { LoginPage } from "../pages/LoginPage";
+import { InvoiceListPage } from "../pages/InvoiceListPage";
 
 test.describe("Create New Invoice Via Invoice Duplication Feature", () => {
-  let loginPage;
-  let invoiceListPage;
+  let loginPage: LoginPage;
+  let invoiceListPage: InvoiceListPage;
 
   test.beforeEach(async ({ page }) => {
     loginPage = new LoginPage(page);
@@ -15,19 +16,19 @@ test.describe("Create New Invoice Via Invoice Duplication Feature", () => {
     page,
   }) => {
     await invoiceListPage.visit(2020);
-    let duplicateInvoicePage =
+    const duplicateInvoicePage =
       await invoiceListPage.clickDuplicateInvoiceNumber("202001-007");
     await duplicateInvoicePage.editInvoiceNumber("202001-008");
     await duplicateInvoicePage.editInvoiceDate("2020-01-01");
     await duplicateInvoicePage.editFirstItem("Technical coach", "1000", "12");
     await duplicateInvoicePage.editSecondItem("UX", "2000", "10");
-    let viewInvoicePage = await duplicateInvoicePage.save();
+    const viewInvoicePage = await duplicateInvoicePage.save();
     await viewInvoicePage.containsInvoiceNumber();
     await viewInvoicePage.containsFirstItem("Technical coach", "1000", "12");
 
     await invoiceListPage.visit(2020);
     await invoiceListPage.containsInvoice("202001-008");
-    let editInvoicePage = await invoiceListPage.clickEditInvoiceNumber(
+    const editInvoicePage = await invoiceListPage.clickEditInvoiceNumber(
       "202001-008"
     );
     await editInvoicePage.editInvoiceDate("2020-01-02");
