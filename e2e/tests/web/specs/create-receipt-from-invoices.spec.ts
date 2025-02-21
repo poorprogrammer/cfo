4902b46ebb2751122029f79764f06c1ce4eadefe
juacompe <juacompe@gmail.com>
2025-02-21 17:54:26 +0700

    refactor: expose item details

--- e2e/tests/web/specs/create-receipt-from-invoices.spec.ts
+++ e2e/tests/web/specs/create-receipt-from-invoices.spec.ts
@@ -11,13 +11,29 @@ test.describe("Create New Receipt For Multiple Invoices", () => {
   });
 
   test("Create new Receipt from multiple invoices", async ({ page }) => {
-    await duplicateInvoice({ from: "202001-007", to: "202001-008" });
-    await createReceiptFromInvoices({
-      invoices: ["202001-007", "202001-008"],
-      receiptNumber: "R202001-002",
+    await duplicateInvoice({
+      from: "202001-007",
+      to: "202001-008",
+      date: "2020-01-01",
+      items: [
+        { description: "Technical coach", rate: "1000", quantity: "12" },
+        { description: "UX", rate: "2000", quantity: "10" },
+      ],
     });
-    await deleteInvoice("202001-008");
-    await deleteReceipt(page, "R202001-002");
+
+    await createReceipt({
+      invoices: ["202001-007", "202001-008"],
+      number: "R202001-002",
+      expectedAmount: "THB 432,800.00",
+    });
+
+    await cleanupDocuments(
+      {
+        invoice: "202001-008",
+        receipt: "R202001-002",
+      },
+      page
+    );
   });
 
   async function login(page: Page) {
@@ -26,36 +42,77 @@ test.describe("Create New Receipt For Multiple Invoices", () => {
     invoiceArchivePage = await loginPage.login();
   }
 
-  async function duplicateInvoice({ from, to }: { from: string; to: string }) {
+  async function duplicateInvoice({
+    from,
+    to,
+    date,
+    items,
+  }: {
+    from: string;
+    to: string;
+    date: string;
+    items: Array<{ description: string; rate: string; quantity: string }>;
+  }) {
     await invoiceArchivePage.visit(2020);
     const duplicatePage = await invoiceArchivePage.clickDuplicateDocumentNumber(
       from
     );
     await duplicatePage.editDocumentNumber(to);
-    await duplicatePage.editDocumentDate("2020-01-01");
-    await duplicatePage.editFirstItem("Technical coach", "1000", "12");
-    await duplicatePage.editSecondItem("UX", "2000", "10");
+    await duplicatePage.editDocumentDate(date);
+    await duplicatePage.editFirstItem(
+      items[0].description,
+      items[0].rate,
+      items[0].quantity
+    );
+    await duplicatePage.editSecondItem(
+      items[1].description,
+      items[1].rate,
+      items[1].quantity
+    );
+
     const viewInvoicePage = await duplicatePage.save();
     await viewInvoicePage.containsDocumentNumber(to);
-    await viewInvoicePage.containsFirstItem("Technical coach", "1000", "12");
+    await viewInvoicePage.containsFirstItem(
+      items[0].description,
+      items[0].rate,
+      items[0].quantity
+    );
     await viewInvoicePage.containsText("THB 32,000.00");
   }
 
-  async function createReceiptFromInvoices({
+  async function createReceipt({
     invoices,
-    receiptNumber,
+    number,
+    expectedAmount,
   }: {
     invoices: string[];
-    receiptNumber: string;
+    number: string;
+    expectedAmount: string;
   }) {
     await invoiceArchivePage.visit(2020);
     await invoiceArchivePage.containsDocument("202001-008");
+
     const createReceiptPage =
       await invoiceArchivePage.createReceiptFromInvoices(invoices);
-    await createReceiptPage.editDocumentNumber(receiptNumber);
+    await createReceiptPage.editDocumentNumber(number);
+
     const viewReceiptPage = await createReceiptPage.save();
-    await viewReceiptPage.containsDocumentNumber("R202001-002");
-    await viewReceiptPage.containsText("THB 432,800.00");
+    await viewReceiptPage.containsDocumentNumber(number);
+    await viewReceiptPage.containsText(expectedAmount);
+  }
+
+  async function cleanupDocuments(
+    {
+      invoice,
+      receipt,
+    }: {
+      invoice: string;
+      receipt: string;
+    },
+    page: Page
+  ) {
+    await deleteInvoice(invoice);
+    await deleteReceipt(receipt, page);
   }
 
   async function deleteInvoice(invoice: string) {
@@ -65,7 +122,7 @@ test.describe("Create New Receipt For Multiple Invoices", () => {
     await invoiceArchivePage.shouldNotContainDocument(invoice);
   }
 
-  async function deleteReceipt(page: Page, receipt: string) {
+  async function deleteReceipt(receipt: string, page: Page) {
     const receiptArchivePage = new BillingArchivePage(page, "Receipt");
     await receiptArchivePage.visit(new Date().getFullYear());
     await receiptArchivePage.containsDocument(receipt);
