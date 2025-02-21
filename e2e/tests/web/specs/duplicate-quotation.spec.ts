5b8589eb7e3773aae3b29dab84bab39cf39b275a
juacompe <juacompe@gmail.com>
2025-02-21 17:52:47 +0700

    refactor: encapsulate technical details

--- e2e/tests/web/specs/duplicate-quotation.spec.ts
+++ e2e/tests/web/specs/duplicate-quotation.spec.ts
@@ -1,4 +1,4 @@
-import { test } from "@playwright/test";
+import { Page, test } from "@playwright/test";
 import { LoginPage } from "../pages/LoginPage";
 import BillingArchivePage from "../pages/BillingArchivePage";
 
@@ -7,38 +7,100 @@ test.describe("Create New Quotation Via Duplication Feature", () => {
   let archivePage: BillingArchivePage;
 
   test.beforeEach(async ({ page }) => {
-    loginPage = new LoginPage(page);
-    await loginPage.goto();
-    archivePage = await loginPage.login();
-    archivePage = new BillingArchivePage(page, "Quotation");
+    await login(page);
   });
 
   test("Can create new Quotation by duplicate from existing one", async ({
     page,
   }) => {
+    await duplicateQuotation({
+      from: "202001-001",
+      to: "202001-008",
+      date: "2020-01-01",
+      items: [
+        { description: "Technical coach", rate: "1000.5", quantity: "12.5" },
+      ],
+    });
+
+    await editQuotation({
+      number: "202001-008",
+      date: "2020-01-02",
+      items: [
+        { description: "Fullstack developer", rate: "1000", quantity: "10" },
+      ],
+    });
+
+    await deleteQuotation("202001-008");
+  });
+
+  async function login(page: Page) {
+    loginPage = new LoginPage(page);
+    await loginPage.goto();
+    archivePage = await loginPage.login();
+    archivePage = new BillingArchivePage(page, "Quotation");
+  }
+
+  async function duplicateQuotation({
+    from,
+    to,
+    date,
+    items,
+  }: {
+    from: string;
+    to: string;
+    date: string;
+    items: Array<{ description: string; rate: string; quantity: string }>;
+  }) {
     await archivePage.visit(2020);
-    const duplicatePage = await archivePage.clickDuplicateDocumentNumber(
-      "202001-001"
+    const duplicatePage = await archivePage.clickDuplicateDocumentNumber(from);
+
+    await duplicatePage.editDocumentNumber(to);
+    await duplicatePage.editDocumentDate(date);
+    await duplicatePage.editFirstItem(
+      items[0].description,
+      items[0].rate,
+      items[0].quantity
     );
-    await duplicatePage.editDocumentNumber("202001-008");
-    await duplicatePage.editDocumentDate("2020-01-01");
-    await duplicatePage.editFirstItem("Technical coach", "1000.5", "12.5");
+
     const viewPage = await duplicatePage.save();
     await viewPage.containsDocumentNumber("008");
-    await viewPage.containsFirstItem("Technical coach", "1,000.50", "12.5");
+    await viewPage.containsFirstItem(
+      items[0].description,
+      "1,000.50",
+      items[0].quantity
+    );
     await viewPage.containsText("USD 12,506.25");
+  }
 
+  async function editQuotation({
+    number,
+    date,
+    items,
+  }: {
+    number: string;
+    date: string;
+    items: Array<{ description: string; rate: string; quantity: string }>;
+  }) {
     await archivePage.visit(2020);
-    await archivePage.containsDocument("202001-008");
-    const editPage = await archivePage.clickEditDocumentNumber("202001-008");
-    await editPage.editDocumentDate("2020-01-02");
-    await editPage.editFirstItem("Fullstack developer", "1000", "10");
-    await editPage.save();
-    await viewPage.containsDocumentNumber("202001-008");
+    await archivePage.containsDocument(number);
+
+    const editPage = await archivePage.clickEditDocumentNumber(number);
+    await editPage.editDocumentDate(date);
+    await editPage.editFirstItem(
+      items[0].description,
+      items[0].rate,
+      items[0].quantity
+    );
+
+    const viewPage = await editPage.save();
+    await viewPage.containsDocumentNumber(number);
     await viewPage.containsText("USD 10,000.00");
+  }
+
+  async function deleteQuotation(quotation: string) {
     await archivePage.visit(2020);
-    await archivePage.containsDocument("202001-008");
-    await archivePage.delete("202001-008");
-    await archivePage.shouldNotContainDocument("202001-008");
-  });
+    await archivePage.containsDocument(quotation);
+    await archivePage.delete(quotation);
+    await archivePage.shouldNotContainDocument(quotation);
+  }
 });
--- e2e/tests/web/specs/duplicate-receipt.spec.ts
+++ e2e/tests/web/specs/duplicate-receipt.spec.ts
@@ -1,4 +1,4 @@
-import { test } from "@playwright/test";
+import { Page, test } from "@playwright/test";
 import { LoginPage } from "../pages/LoginPage";
 import BillingArchivePage from "../pages/BillingArchivePage";
 import DuplicateReceiptPage from "../pages/DuplicateReceiptPage";
@@ -8,38 +8,100 @@ test.describe("Create New Receipt Via Receipt Duplication Feature", () => {
   let archivePage: BillingArchivePage;
 
   test.beforeEach(async ({ page }) => {
-    loginPage = new LoginPage(page);
-    await loginPage.goto();
-    archivePage = await loginPage.login();
-    archivePage = new BillingArchivePage(page, "Receipt");
+    await login(page);
   });
 
   test("Can create new Receipt by duplicate from existing one", async ({
     page,
   }) => {
-    await archivePage.visit(2020);
-    await archivePage.clickDuplicateDocumentNumber("202001-001");
-    const duplicatePage = new DuplicateReceiptPage(page);
-    await duplicatePage.editDocumentNumber("202001-008");
-    await duplicatePage.editDocumentDate("2020-01-01");
-    await duplicatePage.editFirstItem("Technical coach", "1000", "12");
-    const viewPage = await duplicatePage.save();
-    await viewPage.containsDocumentNumber("202001-008");
-    await viewPage.containsFirstItem("Technical coach", "1000", "12");
-    await viewPage.containsText("USD 12,000.00");
+    await duplicateReceipt({
+      from: "202001-001",
+      to: "202001-008",
+      date: "2020-01-01",
+      items: [{ description: "Technical coach", rate: "1000", quantity: "12" }],
+    });
 
-    await archivePage.visit(2020);
-    await archivePage.containsDocument("202001-008");
-    await archivePage.clickEditDocumentNumber("202001-008");
-    const editPage = new DuplicateReceiptPage(page);
-    await editPage.editDocumentDate("2020-01-02");
-    await editPage.editFirstItem("Fullstack developer", "1000", "10");
-    await editPage.save();
-    await viewPage.containsDocumentNumber("202001-008");
-    await viewPage.containsText("USD 10,000.00");
-    await archivePage.visit(2020);
-    await archivePage.containsDocument("202001-008");
-    await archivePage.delete("202001-008");
-    await archivePage.shouldNotContainDocument("202001-008");
+    await editReceipt({
+      number: "202001-008",
+      date: "2020-01-02",
+      items: [
+        { description: "Fullstack developer", rate: "1000", quantity: "10" },
+      ],
+    });
+
+    await deleteReceipt("202001-008");
   });
+
+  async function login(page: Page) {
+    loginPage = new LoginPage(page);
+    await loginPage.goto();
+    archivePage = await loginPage.login();
+    archivePage = new BillingArchivePage(page, "Receipt");
+  }
+
+  async function duplicateReceipt({
+    from,
+    to,
+    date,
+    items,
+  }: {
+    from: string;
+    to: string;
+    date: string;
+    items: Array<{ description: string; rate: string; quantity: string }>;
+  }) {
+    await archivePage.visit(2020);
+    await archivePage.clickDuplicateDocumentNumber(from);
+
+    const duplicatePage = new DuplicateReceiptPage(page);
+    await duplicatePage.editDocumentNumber(to);
+    await duplicatePage.editDocumentDate(date);
+    await duplicatePage.editFirstItem(
+      items[0].description,
+      items[0].rate,
+      items[0].quantity
+    );
+
+    const viewPage = await duplicatePage.save();
+    await viewPage.containsDocumentNumber(to);
+    await viewPage.containsFirstItem(
+      items[0].description,
+      items[0].rate,
+      items[0].quantity
+    );
+    await viewPage.containsText("USD 12,000.00");
+  }
+
+  async function editReceipt({
+    number,
+    date,
+    items,
+  }: {
+    number: string;
+    date: string;
+    items: Array<{ description: string; rate: string; quantity: string }>;
+  }) {
+    await archivePage.visit(2020);
+    await archivePage.containsDocument(number);
+    await archivePage.clickEditDocumentNumber(number);
+
+    const editPage = new DuplicateReceiptPage(page);
+    await editPage.editDocumentDate(date);
+    await editPage.editFirstItem(
+      items[0].description,
+      items[0].rate,
+      items[0].quantity
+    );
+
+    const viewPage = await editPage.save();
+    await viewPage.containsDocumentNumber(number);
+    await viewPage.containsText("USD 10,000.00");
+  }
+
+  async function deleteReceipt(receipt: string) {
+    await archivePage.visit(2020);
+    await archivePage.containsDocument(receipt);
+    await archivePage.delete(receipt);
+    await archivePage.shouldNotContainDocument(receipt);
+  }
 });
