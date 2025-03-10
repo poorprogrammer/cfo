dae1d16ded7d1003e45cae89db5e915884a597e8
juacompe <juacompe@gmail.com>
2025-03-10 17:49:19 +0700

    add path in 404

--- web/src/views/PageNotFound.vue
+++ web/src/views/PageNotFound.vue
@@ -1,5 +1,8 @@
 <template>
   <div>
     <h1>Page not found !</h1>
+    <p>The page you are looking for does not exist.</p>
+    <h4>Path:</h4>
+    <p>{{ $route.fullPath }}</p>
   </div>
 </template>
