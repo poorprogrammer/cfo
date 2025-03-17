e9407062ed5d74f5de2fcbe00ef137a30c76a94f
Yongyut Srisuban <yongyut120@odds.team>
2025-03-18 00:21:18 +0700

    refactor: add type annotations to constructor and methods in BillingArchivePage

--- e2e/tests/web/pages/BillingArchivePage.ts
+++ e2e/tests/web/pages/BillingArchivePage.ts
@@ -8,14 +8,14 @@ export default class BillingArchivePage {
   documentType: string;
   generateReceiptButton: Locator;
 
-  constructor(page: Page, documentType) {
+  constructor(page: Page, documentType: string) {
     this.page = page;
     this.confirmDeleteButton = page.locator(".delete-btn");
     this.documentType = documentType;
     this.generateReceiptButton = page.locator("#generate-receipt");
   }
 
-  static async create(page, documentType) {
+  static async create(page: Page, documentType: string) {
     let archivePage = new BillingArchivePage(page, documentType);
     await archivePage.ready();
     return archivePage;
@@ -29,26 +29,26 @@ export default class BillingArchivePage {
     await this.page.goto(`/${this.documentType.toLocaleLowerCase()}s/${year}`);
   }
 
-  async clickDuplicateDocumentNumber(documentNumber) {
+  async clickDuplicateDocumentNumber(documentNumber: string) {
     await this.page.locator(`#duplicate_${documentNumber}`).click();
     return DuplicateDocumentPage.create(this.page, this.documentType);
   }
 
-  async clickEditDocumentNumber(documentNumber) {
+  async clickEditDocumentNumber(documentNumber: string) {
     await this.page.locator(`#edit_${documentNumber}`).click();
     return DuplicateDocumentPage.create(this.page, this.documentType);
   }
 
-  async delete(documentNumber) {
+  async delete(documentNumber: string) {
     await this.page.locator(`#delete_${documentNumber}`).click();
     await this.confirmDeleteButton.click();
   }
 
-  async containsDocument(documentNumber) {
+  async containsDocument(documentNumber: string) {
     await expect(this.page.getByText(`${documentNumber}`)).toBeVisible();
   }
 
-  async shouldNotContainDocument(documentNumber) {
+  async shouldNotContainDocument(documentNumber: string) {
     await expect(this.page.getByText(`${documentNumber}`)).not.toBeVisible();
   }
 
