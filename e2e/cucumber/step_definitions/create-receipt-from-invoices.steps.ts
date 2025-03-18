004602a142f438ef7021c12861c72dfab175b1c8
Yongyut Srisuban <yongyut120@odds.team>
2025-03-18 20:51:44 +0700

    refactor: simplify login step definition by utilizing CustomWorld

--- e2e/cucumber/step_definitions/create-receipt-from-invoices.steps.ts
+++ e2e/cucumber/step_definitions/create-receipt-from-invoices.steps.ts
@@ -1,18 +1,6 @@
-import { Given, When } from "@cucumber/cucumber";
-import { Page, Browser, chromium } from "@playwright/test";
-import { LoginPage } from "../../tests/web/pages/LoginPage";
-import BillingArchivePage from "../../tests/web/pages/BillingArchivePage";
+import { Given } from "@cucumber/cucumber";
+import { CustomWorld } from "../support/world";
 
-let loginPage: LoginPage;
-let invoiceArchivePage: BillingArchivePage;
-let page: Page;
-let browser: Browser;
-
-Given("I am logged in", async function () {
-  browser = await chromium.launch();
-  page = await browser.newPage();
-  page = await browser.newPage();
-  loginPage = new LoginPage(page);
-  await loginPage.goto();
-  invoiceArchivePage = await loginPage.login();
+Given("I am logged in", async function (this: CustomWorld) {
+  await this.loginPage.goto();
 });
