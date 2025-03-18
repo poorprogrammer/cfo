d0032507dec9ac80d2a38df286e395b59b9bf703
Yongyut Srisuban <yongyut120@odds.team>
2025-03-18 21:25:59 +0700

    feat: add step definitions

--- e2e/cucumber/step_definitions/create-receipt-from-invoices.steps.ts
+++ e2e/cucumber/step_definitions/create-receipt-from-invoices.steps.ts
@@ -1,6 +1,38 @@
-import { Given } from "@cucumber/cucumber";
+import { Given, When, Then } from "@cucumber/cucumber";
 import { CustomWorld } from "../support/world";
 
 Given("I am logged in", async function (this: CustomWorld) {
   await this.loginPage.goto();
 });
+
+When(
+  "I duplicate invoice {string} to {string} with date {string} and items:",
+  async function (string, string2, string3, dataTable) {
+    // Write code here that turns the phrase above into concrete actions
+    return "pending";
+  }
+);
+
+When(
+  "I create a receipt from invoices {string} and {string} with number {string}",
+  async function (string, string2, string3) {
+    // Write code here that turns the phrase above into concrete actions
+    return "pending";
+  }
+);
+
+Then(
+  "I should see the receipt with number {string} and amount {string}",
+  async function (string, string2) {
+    // Write code here that turns the phrase above into concrete actions
+    return "pending";
+  }
+);
+
+Then(
+  "I cleanup documents with invoice {string} and receipt {string}",
+  async function (string, string2) {
+    // Write code here that turns the phrase above into concrete actions
+    return "pending";
+  }
+);
