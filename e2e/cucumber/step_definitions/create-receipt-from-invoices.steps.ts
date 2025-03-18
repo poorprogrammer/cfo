7eb260ab66655b4caed28b1ec1889db337a978fb
Yongyut Srisuban <yongyut120@odds.team>
2025-03-18 22:17:03 +0700

    feat: implement invoice duplication functionality in Cucumber step definitions

--- e2e/cucumber/step_definitions/create-receipt-from-invoices.steps.ts
+++ e2e/cucumber/step_definitions/create-receipt-from-invoices.steps.ts
@@ -8,9 +8,43 @@ Given("I am logged in", async function (this: CustomWorld) {
 
 When(
   "I duplicate invoice {string} to {string} with date {string} and items:",
-  async function (string, string2, string3, dataTable) {
-    // Write code here that turns the phrase above into concrete actions
-    return "pending";
+  async function (
+    this: CustomWorld,
+    from: string,
+    to: string,
+    date: string,
+    dataTable: { rawTable: Array<Array<string>> }
+  ) {
+    const items = dataTable.rawTable.slice(1).map((row) => ({
+      description: row[0],
+      rate: row[1],
+      quantity: row[2],
+    }));
+
+    await this.invoiceArchivePage.visit(2020);
+    const duplicatePage =
+      await this.invoiceArchivePage.clickDuplicateDocumentNumber(from);
+    await duplicatePage.editDocumentNumber(to);
+    await duplicatePage.editDocumentDate(date);
+    console.log(items);
+    await duplicatePage.editFirstItem(
+      items[0].description,
+      items[0].rate,
+      items[0].quantity
+    );
+    await duplicatePage.editSecondItem(
+      items[1].description,
+      items[1].rate,
+      items[1].quantity
+    );
+    const viewInvoicePage = await duplicatePage.save();
+    await viewInvoicePage.containsDocumentNumber(to);
+    await viewInvoicePage.containsFirstItem(
+      items[0].description,
+      items[0].rate,
+      items[0].quantity
+    );
+    await viewInvoicePage.containsText("THB 32,000.00");
   }
 );
 
