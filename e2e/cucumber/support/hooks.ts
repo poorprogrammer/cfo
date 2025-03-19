c1dbb970b2e30bea0e1a3a86f9967bf279cbb82e
Yongyut Srisuban <yongyut120@odds.team>
2025-03-19 16:54:40 +0700

    feat: add line breaks to browser launch and closure logs in Cucumber hooks

--- e2e/cucumber/support/hooks.ts
+++ e2e/cucumber/support/hooks.ts
@@ -2,11 +2,11 @@ import { Before, After } from "@cucumber/cucumber";
 import { CustomWorld } from "./world";
 
 Before(async function (this: CustomWorld) {
-  console.log("🚀 Launching Browser for Scenario...");
+  console.log("🚀 Launching Browser for Scenario...\n\n");
   await this.launchBrowser();
 });
 
 After(async function (this: CustomWorld) {
-  console.log("🛑 Closing Browser after Scenario...");
   await this.closeBrowser();
+  console.log("\n\n🛑 Closing Browser after Scenario...");
 });
