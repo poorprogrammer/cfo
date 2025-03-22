e5be04950246e2b166624be2174c6ebf63d563a9
Yongyut Srisuban <yongyut120@odds.team>
2025-03-23 03:17:09 +0700

    fix: adjust console log formatting in browser launch and closure hooks

--- e2e/cucumber/support/hooks.ts
+++ e2e/cucumber/support/hooks.ts
@@ -2,11 +2,11 @@ import { Before, After } from "@cucumber/cucumber";
 import { CustomWorld } from "./world";
 
 Before(async function (this: CustomWorld) {
-  console.log("🚀 Launching Browser for Scenario...\n\n");
+  console.log("🚀 Launching Browser for Scenario...\n");
   await this.launchBrowser();
 });
 
 After(async function (this: CustomWorld) {
   await this.closeBrowser();
-  console.log("\n\n🛑 Closing Browser after Scenario...");
+  console.log("\n🛑 Closing Browser after Scenario...");
 });
